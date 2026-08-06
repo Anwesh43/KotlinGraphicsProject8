f204741007619a63a8ca0a1dd97b139bad9490bc
Anwesh43 <anweshthecool0@gmail.com>
2026-08-06 13:14:12 +0530

    arcconcrotdownview: adding Canvas extension methods

--- arcconcrotdownview/src/main/java/com/example/arcconcrotdownview/ArcConcRotDownView.kt
+++ arcconcrotdownview/src/main/java/com/example/arcconcrotdownview/ArcConcRotDownView.kt
@@ -28,3 +28,36 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcConcRotDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            val r : Float = size / (1 + j)
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawArc(RectF(-r, -r, r, r), -90f, 90f * dsc(j), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawACRDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawArcConcRotDown(scale, w, h, paint)
+}
