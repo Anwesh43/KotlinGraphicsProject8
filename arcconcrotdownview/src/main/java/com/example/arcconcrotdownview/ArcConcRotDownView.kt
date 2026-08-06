1d918cc19d3fb4fca5d4a8506acc25336bac6beb
Anwesh43 <anweshthecool0@gmail.com>
2026-08-06 13:27:28 +0530

    arcconcrotdownview: adding Renderer class

--- arcconcrotdownview/src/main/java/com/example/arcconcrotdownview/ArcConcRotDownView.kt
+++ arcconcrotdownview/src/main/java/com/example/arcconcrotdownview/ArcConcRotDownView.kt
@@ -188,4 +188,27 @@ class ArcConcRotDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ArcConcRotDownView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val acrd : ArcConcRotDown = ArcConcRotDown(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            acrd.draw(canvas, paint)
+            animator.animate {
+                acrd.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            acrd.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
