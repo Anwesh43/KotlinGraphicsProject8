c5d606370cb48f0779edc4465518e4bc9d907e25
Anwesh43 <anweshthecool0@gmail.com>
2026-08-06 13:15:09 +0530

    arcconcrotdownview: adding ArcConcRotDownView class

--- arcconcrotdownview/src/main/java/com/example/arcconcrotdownview/ArcConcRotDownView.kt
+++ arcconcrotdownview/src/main/java/com/example/arcconcrotdownview/ArcConcRotDownView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawACRDNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawArcConcRotDown(scale, w, h, paint)
 }
+
+class ArcConcRotDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
