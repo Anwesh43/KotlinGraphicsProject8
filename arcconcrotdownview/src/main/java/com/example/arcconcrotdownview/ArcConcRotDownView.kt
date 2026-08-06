888eb68be047b8c5f6881c3cc8af8cbd0a40cea9
Anwesh43 <anweshthecool0@gmail.com>
2026-08-06 13:24:43 +0530

    arcconcrotdownview: adding ACRDNode class

--- arcconcrotdownview/src/main/java/com/example/arcconcrotdownview/ArcConcRotDownView.kt
+++ arcconcrotdownview/src/main/java/com/example/arcconcrotdownview/ArcConcRotDownView.kt
@@ -124,4 +124,45 @@ class ArcConcRotDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ACRDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ACRDNode? = null
+        private var prev : ACRDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ACRDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawACRDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ACRDNode {
+            var curr : ACRDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
