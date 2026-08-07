ed3453ca7da3132ea34ff408683e25cd1821ed88
Anwesh43 <anweshthecool0@gmail.com>
2026-08-07 15:03:13 +0530

    linebentsidesweepview: adding LineBentSideSweepView class

--- linebentsidesweepview/src/main/java/com/example/linebentsidesweepview/LineBentSideSweepView.kt
+++ linebentsidesweepview/src/main/java/com/example/linebentsidesweepview/LineBentSideSweepView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawLBSSNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineBentSideSweep(scale, w, h, paint)
 }
+
+class LineBentSideSweepView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN ->{
+
+            }
+        }
+        return true
+    }
+}
