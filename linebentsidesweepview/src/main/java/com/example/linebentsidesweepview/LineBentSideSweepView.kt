f8a58c0a12af21590b591342524db39f7a4902d2
Anwesh43 <anweshthecool0@gmail.com>
2026-08-07 15:33:27 +0530

    linebentsidesweepview: adding LBSSNode class

--- linebentsidesweepview/src/main/java/com/example/linebentsidesweepview/LineBentSideSweepView.kt
+++ linebentsidesweepview/src/main/java/com/example/linebentsidesweepview/LineBentSideSweepView.kt
@@ -126,4 +126,45 @@ class LineBentSideSweepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBSSNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBSSNode? = null
+        private var prev : LBSSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBSSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBSSNode(i, state.scale, paint)
+        }
+
+        fun update(cb: (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBSSNode {
+            var curr : LBSSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
