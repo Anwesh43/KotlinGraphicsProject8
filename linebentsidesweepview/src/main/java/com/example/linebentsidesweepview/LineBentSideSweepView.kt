2b352beb8ad7949de248cc20dc64da2f61329def
Anwesh43 <anweshthecool0@gmail.com>
2026-08-07 14:53:30 +0530

    linebentsidesweepview: adding Canvas extensiion methods

--- linebentsidesweepview/src/main/java/com/example/linebentsidesweepview/LineBentSideSweepView.kt
+++ linebentsidesweepview/src/main/java/com/example/linebentsidesweepview/LineBentSideSweepView.kt
@@ -23,7 +23,43 @@ val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
 val rot : Float = -270f
+val bentDeg : Float = 30f
+val sweep : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBentSideSweep(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(5)) {
+        rotate(sweep * dsc(4))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(bentDeg * dsc(1) * j + rot * (1 - j) * dsc(3))
+                drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+            }
+        }
+        drawArc(RectF(-size, -size, size, size), -(90f - bentDeg), (90 - bentDeg) * dsc(2), false, paint)
+    }
+}
+
+fun Canvas.drawLBSSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineBentSideSweep(scale, w, h, paint)
+}
