6108c86fbb2eac4444d34fb18c4f96de392524f6
Anwesh43 <anweshthecool0@gmail.com>
2026-08-08 12:36:12 +0530

    linesweepencloseleftview: adding Canvas extension methods

--- linesweepencloseleftview/src/main/java/com/example/linesweepencloseleftview/LineSweepEncloseLeftView.kt
+++ linesweepencloseleftview/src/main/java/com/example/linesweepencloseleftview/LineSweepEncloseLeftView.kt
@@ -24,7 +24,44 @@ val backColor : Int = "#BDBDBD".toColorInt()
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val bentDeg : Float = 60f
+val sweep : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineSweepEncloseLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(5)) {
+        rotate(sweep * dsc(4))
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(3))
+            drawLine(0f, 0f, 0f, -size * dsc(2), paint)
+        }
+        drawXY(0f, 0f) {
+            rotate(bentDeg * dsc(1))
+            drawLine(0f, 0f, size * dsc(0), 0f, paint)
+        }
+        drawArc(RectF(-size, -size, size, size), 180f, bentDeg * dsc(1), false, paint)
+    }
+}
+
+fun Canvas.drawLSELNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineSweepEncloseLeft(scale, w, h, paint)
+}
