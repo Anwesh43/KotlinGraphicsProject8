e91b7cd01bb6c39b7fb4f643e09837ed5e4f0054
Anwesh43 <anweshthecool0@gmail.com>
2026-08-08 12:37:25 +0530

    linesweepencloseleftview: adding LineSweepEncloseLeftView cclass

--- linesweepencloseleftview/src/main/java/com/example/linesweepencloseleftview/LineSweepEncloseLeftView.kt
+++ linesweepencloseleftview/src/main/java/com/example/linesweepencloseleftview/LineSweepEncloseLeftView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawLSELNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineSweepEncloseLeft(scale, w, h, paint)
 }
+
+class LineSweepEncloseLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
