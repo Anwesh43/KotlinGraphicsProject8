a2e897fa3f8d8e351948d982f37a6fd0cf390b14
Anwesh43 <anweshthecool0@gmail.com>
2026-08-08 12:49:26 +0530

    linesweepencloseleftview: adding LineSweepEncloseLeft class

--- linesweepencloseleftview/src/main/java/com/example/linesweepencloseleftview/LineSweepEncloseLeftView.kt
+++ linesweepencloseleftview/src/main/java/com/example/linesweepencloseleftview/LineSweepEncloseLeftView.kt
@@ -169,4 +169,27 @@ class LineSweepEncloseLeftView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineSweepEncloseLeft(var i : Int) {
+
+        private var curr : LSELNode = LSELNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
