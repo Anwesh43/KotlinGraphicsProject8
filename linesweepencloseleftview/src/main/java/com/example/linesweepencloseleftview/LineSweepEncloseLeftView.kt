895da2bc36eeb8c92145d5025c7b283469190007
Anwesh43 <anweshthecool0@gmail.com>
2026-08-08 12:47:26 +0530

    linesweepencloseleftview: adding LSELNode class

--- linesweepencloseleftview/src/main/java/com/example/linesweepencloseleftview/LineSweepEncloseLeftView.kt
+++ linesweepencloseleftview/src/main/java/com/example/linesweepencloseleftview/LineSweepEncloseLeftView.kt
@@ -128,4 +128,45 @@ class LineSweepEncloseLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LSELNode(var i : Int, val state : State = State()) {
+
+        private var next : LSELNode? = null
+        private var prev : LSELNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LSELNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLSELNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LSELNode {
+            var curr : LSELNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
