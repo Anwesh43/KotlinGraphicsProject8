69b4c7f41cde85c50b9ea040c03e7a37e10ee9ed
Anwesh43 <anweshthecool0@gmail.com>
2026-08-08 12:28:14 +0530

    linesweepencloseleftview: adding constants and libraries

---
+++ linesweepencloseleftview/src/main/java/com/example/linesweepencloseleftview/LineSweepEncloseLeftView.kt
@@ -0,0 +1,26 @@
+package com.example.linesweepencloseleftview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Paint
+import androidx.core.graphics.toColorInt
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 6
+val scGap : Float = 0.05f / parts
+val rot : Float = 270f
+val delay : Long = 20
+val backColor : Int = "#BDBDBD".toColorInt()
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 5.9f
+val bentDeg : Float = 60f
