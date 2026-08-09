9f49a781bcf1c702b2a39e39de82937daab6b7e9
Anwesh43 <anweshthecool0@gmail.com>
2026-08-09 22:10:40 +0530

    squprightdownview: adding Canvas extension methods

--- squprightdownview/src/main/java/com/example/squprightdownview/SqUpRigthDownView.kt
+++ squprightdownview/src/main/java/com/example/squprightdownview/SqUpRigthDownView.kt
@@ -27,3 +27,31 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i  * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSqUpRightDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 - size) * dsc(2), h / 2 - h * 0.5f * (1 - dsc(0)) + h * 0.5f * dsc(3)) {
+        rotate(rot * dsc(1))
+        drawRect(RectF(-size, -size, size, size), paint)
+    }
+}
+
+fun Canvas.drawSURDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawSqUpRightDown(scale, w, h, paint)
+}
