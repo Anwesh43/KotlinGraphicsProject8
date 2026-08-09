9531b0f8c1a5e570fc8af235ae4dfb3fef6b9d6e
Anwesh43 <anweshthecool0@gmail.com>
2026-08-09 22:11:47 +0530

    squprightdownview: adding SqUpRightDownView class

--- squprightdownview/src/main/java/com/example/squprightdownview/SqUpRigthDownView.kt
+++ squprightdownview/src/main/java/com/example/squprightdownview/SqUpRigthDownView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawSURDNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawSqUpRightDown(scale, w, h, paint)
 }
+
+class SqUpRightDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
