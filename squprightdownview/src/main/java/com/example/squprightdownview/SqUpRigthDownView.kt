7476688e31d145635dd5b2c3f59f519a288e582c
Anwesh43 <anweshthecool0@gmail.com>
2026-08-09 22:18:34 +0530

    squprightdownview: adding SqUpRightUpDown class

--- squprightdownview/src/main/java/com/example/squprightdownview/SqUpRigthDownView.kt
+++ squprightdownview/src/main/java/com/example/squprightdownview/SqUpRigthDownView.kt
@@ -143,12 +143,12 @@ class SqUpRightDownView(ctx : Context) : View(ctx) {
             state.update(cb)
         }
 
-        fun startUpdting(cb : () -> Unit) {
+        fun startUpdating(cb : () -> Unit) {
             state.startUpdating(cb)
         }
 
         fun getNext(dir : Int, cb : () -> Unit) : SURDNode {
-            var curr : SURDNode? = prev
+            var curr: SURDNode? = prev
             if (dir === 1) {
                 curr = next
             }
@@ -158,6 +158,28 @@ class SqUpRightDownView(ctx : Context) : View(ctx) {
             cb()
             return this
         }
+    }
 
+    data class SqUpRightDown(var i : Int) {
+
+        private var curr : SURDNode = SURDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
     }
 }
