c45748c3f8416b56ec3f2e0d0292be21651df5a3
Anwesh43 <anweshthecool0@gmail.com>
2026-08-10 09:03:25 +0530

    linedropbentrightview: adding LineDropBentRightView class

--- linedropbentrightview/src/main/java/com/example/linedropbentrightview/LineDropBentRightView.kt
+++ linedropbentrightview/src/main/java/com/example/linedropbentrightview/LineDropBentRightView.kt
@@ -60,3 +60,19 @@ fun Canvas.dawLDBRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineDropBentRight(scale, w, h, paint)
 }
+
+class LineDropBentRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                2
+            }
+        }
+        return true
+    }
+}
