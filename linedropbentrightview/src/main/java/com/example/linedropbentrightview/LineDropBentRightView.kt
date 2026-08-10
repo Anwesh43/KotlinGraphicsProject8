921fecc9bbfe9094d80a80bcdf41ecf4d3db2148
Anwesh43 <anweshthecool0@gmail.com>
2026-08-10 08:59:26 +0530

    linedropbentrightview: adding Canvas extension methods

--- linedropbentrightview/src/main/java/com/example/linedropbentrightview/LineDropBentRightView.kt
+++ linedropbentrightview/src/main/java/com/example/linedropbentrightview/LineDropBentRightView.kt
@@ -29,3 +29,34 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineDropBentRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * (1 - j) * dsc(3) + bentDeg * (j) * (1 + dsc(2)))
+                drawLine(0f, 0f, 0f, -size * ((1 - j) * dsc(0) + j * dsc(1)), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.dawLDBRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineDropBentRight(scale, w, h, paint)
+}
