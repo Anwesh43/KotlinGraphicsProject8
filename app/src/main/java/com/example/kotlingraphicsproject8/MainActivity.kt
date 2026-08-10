6295ce73074d23a0c46e351abf7d968d182b4f24
Anwesh43 <anweshthecool0@gmail.com>
2026-08-10 09:18:43 +0530

    linedropbentrightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
@@ -12,13 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsproject8.ui.theme.KotlinGraphicsProject8Theme
-import com.example.squprightdownview.SqUpRightDownView
+import com.example.linedropbentrightview.LineDropBentRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        SqUpRightDownView.create(this)
+        LineDropBentRightView.create(this)
     }
 }
 
