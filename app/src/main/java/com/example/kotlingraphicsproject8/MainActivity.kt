5005d9cdc1c1be0f433741de233c0aaf8ea61e23
Anwesh43 <anweshthecool0@gmail.com>
2026-08-13 14:22:24 +0530

    bentlineincreasearcview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.bentlineincreasearcview.BentLineIncreaseArcView
 import com.example.kotlingraphicsproject8.ui.theme.KotlinGraphicsProject8Theme
-import com.example.linedropbentrightview.LineDropBentRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        LineDropBentRightView.create(this)
+        BentLineIncreaseArcView.create(this)
     }
 }
 
