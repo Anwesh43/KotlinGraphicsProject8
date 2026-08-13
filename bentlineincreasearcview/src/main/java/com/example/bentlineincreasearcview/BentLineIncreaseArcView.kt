2df58afa816d81b7929d13c3e2b0630a4f328399
Anwesh43 <anweshthecool0@gmail.com>
2026-08-13 13:26:02 +0530

    bentlineincreasearcview: adding BLIANode class

--- bentlineincreasearcview/src/main/java/com/example/bentlineincreasearcview/BentLineIncreaseArcView.kt
+++ bentlineincreasearcview/src/main/java/com/example/bentlineincreasearcview/BentLineIncreaseArcView.kt
@@ -124,4 +124,45 @@ class BentLineIncreaseArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLIANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BLIANode? = null
+        private var prev : BLIANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLIANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLIANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLIANode {
+            var curr : BLIANode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
