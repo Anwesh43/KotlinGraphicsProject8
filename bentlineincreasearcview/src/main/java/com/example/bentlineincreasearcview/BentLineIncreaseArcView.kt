884609cec3269861d7a68c4c7165238e19c3cfdf
Anwesh43 <anweshthecool0@gmail.com>
2026-08-13 13:01:54 +0530

    bentlineincreasearcview: adding BentLineIncreaseArcView class

--- bentlineincreasearcview/src/main/java/com/example/bentlineincreasearcview/BentLineIncreaseArcView.kt
+++ bentlineincreasearcview/src/main/java/com/example/bentlineincreasearcview/BentLineIncreaseArcView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawBLIANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBentLineIncreaseArc(scale, w, h, paint)
 }
+
+class BentLineIncreaseArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
