3258289c15b86647add9e0434481a53da614b22c
Anwesh43 <anweshthecool0@gmail.com>
2026-08-14 13:14:03 +0530

    bentbiarcdownview: adding constants and libraries

---
+++ bentbiarcdownview/src/main/java/com/example/bentbiarcdownview/BentBiArcDownView.kt
@@ -0,0 +1,26 @@
+package com.example.bentbiarcdownview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import androidx.core.graphics.toColorInt
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 7
+val scGap : Float = 0.05f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 5.9f
+val delay : Long = 20
+val backColor : Int = "#BDBDBD".toColorInt()
+val rot : Float = 90f
+val bentDeg : Float = -45f
