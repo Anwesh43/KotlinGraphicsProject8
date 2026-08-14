86557ef5a078b6363ec4b02cb167f393aa6d9fea
Anwesh43 <anweshthecool0@gmail.com>
2026-08-14 13:56:49 +0530

    bentbiarcdownview: adding BentBiArcDown class

--- bentbiarcdownview/src/main/java/com/example/bentbiarcdownview/BentBiArcDownView.kt
+++ bentbiarcdownview/src/main/java/com/example/bentbiarcdownview/BentBiArcDownView.kt
@@ -194,4 +194,27 @@ class BentBiArcDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BentBiArcDown(var i : Int) {
+
+        private var curr : BBADNode = BBADNode(0)
+        private var dir : Int =1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun udpate(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
