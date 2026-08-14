86a642a36aae82d4f87a197d7b076e0b1800ea40
Anwesh43 <anweshthecool0@gmail.com>
2026-08-14 13:28:29 +0530

    bentbiarcdownview: adding BentBiArcDownView class

--- bentbiarcdownview/src/main/java/com/example/bentbiarcdownview/BentBiArcDownView.kt
+++ bentbiarcdownview/src/main/java/com/example/bentbiarcdownview/BentBiArcDownView.kt
@@ -91,3 +91,19 @@ fun Canvas.drawBBADNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBentBiArcDown(scale, w, h, paint)
 }
+
+class BentBiArcDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
