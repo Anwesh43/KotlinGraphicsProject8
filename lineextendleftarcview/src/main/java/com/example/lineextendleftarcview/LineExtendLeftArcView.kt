bd23045bde871d82432615e5eade456b69deb2cd
Anwesh43 <anweshthecool0@gmail.com>
2026-08-15 11:44:10 +0530

    linextendleftarcview: adding Canvas extension methodd

--- lineextendleftarcview/src/main/java/com/example/lineextendleftarcview/LineExtendLeftArcView.kt
+++ lineextendleftarcview/src/main/java/com/example/lineextendleftarcview/LineExtendLeftArcView.kt
@@ -28,3 +28,38 @@ val deg : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineExtendLeftArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(5), h / 2) {
+        rotate(deg * dsc(4))
+        drawXY(0f, 0f) {
+            rotate(encloseDeg * dsc(3))
+            drawXY(-size, 0f) {
+                drawLine(0f, 0f, size * dsc(0), 0f, paint)
+            }
+            drawLine(0f, 0f, 0f, -size * dsc(1), paint)
+            drawArc(RectF(-size, -size, size, size), -90f, 90f * dsc(2), false, paint)
+        }
+    }
+}
+
+fun Canvas.drawLELANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineExtendLeftArc(scale, w, h, paint)
+}
