450c57113893d940799af274506b7a9b96ef76e7
Anwesh43 <anweshthecool0@gmail.com>
2026-08-15 11:45:19 +0530

    linextendleftarcview: adding LineExtendLeftArcView class

--- lineextendleftarcview/src/main/java/com/example/lineextendleftarcview/LineExtendLeftArcView.kt
+++ lineextendleftarcview/src/main/java/com/example/lineextendleftarcview/LineExtendLeftArcView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawLELANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineExtendLeftArc(scale, w, h, paint)
 }
+
+class LineExtendLeftArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
