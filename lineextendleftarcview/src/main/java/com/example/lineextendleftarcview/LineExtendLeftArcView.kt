3c31e12c9d41f8f344bd9f6b9e821bc3917a40c7
Anwesh43 <anweshthecool0@gmail.com>
2026-08-15 11:52:08 +0530

    linextendleftarcview: adding LineExtendLeftArc class

--- lineextendleftarcview/src/main/java/com/example/lineextendleftarcview/LineExtendLeftArcView.kt
+++ lineextendleftarcview/src/main/java/com/example/lineextendleftarcview/LineExtendLeftArcView.kt
@@ -167,4 +167,27 @@ class LineExtendLeftArcView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineExtendLeftArc(var i : Int) {
+
+        private var curr : LELANode = LELANode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
