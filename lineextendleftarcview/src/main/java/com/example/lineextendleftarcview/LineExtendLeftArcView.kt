c1fa0ab1bf2148131dd436d6a7cdbcf14a38cb4f
Anwesh43 <anweshthecool0@gmail.com>
2026-08-15 11:53:23 +0530

    linextendleftarcview: adding Renderer class

--- lineextendleftarcview/src/main/java/com/example/lineextendleftarcview/LineExtendLeftArcView.kt
+++ lineextendleftarcview/src/main/java/com/example/lineextendleftarcview/LineExtendLeftArcView.kt
@@ -190,4 +190,27 @@ class LineExtendLeftArcView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineExtendLeftArcView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val lela : LineExtendLeftArc = LineExtendLeftArc(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lela.draw(canvas, paint)
+            animator.animate {
+                lela.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lela.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
