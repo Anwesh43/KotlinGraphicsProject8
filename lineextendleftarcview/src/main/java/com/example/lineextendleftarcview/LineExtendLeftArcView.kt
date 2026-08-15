799066d88fe72fbf4401ffca748e2ef5d05b3b6c
Anwesh43 <anweshthecool0@gmail.com>
2026-08-15 11:49:58 +0530

    linextendleftarcview: adding LELANode class

--- lineextendleftarcview/src/main/java/com/example/lineextendleftarcview/LineExtendLeftArcView.kt
+++ lineextendleftarcview/src/main/java/com/example/lineextendleftarcview/LineExtendLeftArcView.kt
@@ -126,4 +126,45 @@ class LineExtendLeftArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LELANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LELANode? = null
+        private var prev : LELANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LELANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLELANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LELANode {
+            var curr : LELANode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
