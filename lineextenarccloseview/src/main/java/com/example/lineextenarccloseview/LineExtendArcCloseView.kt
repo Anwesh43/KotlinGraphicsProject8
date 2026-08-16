9410740dee37f784cc24bd656d31f11dc8a866f8
Anwesh43 <anweshthecool0@gmail.com>
2026-08-16 11:51:48 +0530

    lineextenarccloseview: adding LEACNode class

--- lineextenarccloseview/src/main/java/com/example/lineextenarccloseview/LineExtendArcCloseView.kt
+++ lineextenarccloseview/src/main/java/com/example/lineextenarccloseview/LineExtendArcCloseView.kt
@@ -126,4 +126,45 @@ class LineExtendArcCloseView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LEACNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LEACNode? = null
+        private var prev : LEACNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LEACNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLEACNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LEACNode {
+            var curr : LEACNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
