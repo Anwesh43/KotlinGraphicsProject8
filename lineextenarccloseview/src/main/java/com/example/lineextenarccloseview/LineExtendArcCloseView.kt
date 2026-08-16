006137ea51a07a4b8889ac83ce23d79b2d6e8709
Anwesh43 <anweshthecool0@gmail.com>
2026-08-16 12:23:44 +0530

    lineextenarccloseview: adding static create method

--- lineextenarccloseview/src/main/java/com/example/lineextenarccloseview/LineExtendArcCloseView.kt
+++ lineextenarccloseview/src/main/java/com/example/lineextenarccloseview/LineExtendArcCloseView.kt
@@ -215,4 +215,12 @@ class LineExtendArcCloseView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity : Activity) : LineExtendArcCloseView {
+            val view : LineExtendArcCloseView = LineExtendArcCloseView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
