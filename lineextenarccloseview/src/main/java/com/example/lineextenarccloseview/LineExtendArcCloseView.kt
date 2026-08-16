ac1474eb264dfca4d37dc8bd6503bc1fdb5479c2
Anwesh43 <anweshthecool0@gmail.com>
2026-08-16 11:26:33 +0530

    lineextenarccloseview: adding LineExtendArcCloseView class

--- lineextenarccloseview/src/main/java/com/example/lineextenarccloseview/LineExtendArcCloseView.kt
+++ lineextenarccloseview/src/main/java/com/example/lineextenarccloseview/LineExtendArcCloseView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawLEACNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineExtendArcClose(scale, w, h, paint)
 }
+
+class LineExtendArcCloseView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
