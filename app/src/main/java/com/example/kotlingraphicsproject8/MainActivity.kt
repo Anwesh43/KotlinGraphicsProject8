cc329257551466bbcb847b4ccc89a9527a01d23b
Anwesh43 <anweshthecool0@gmail.com>
2026-08-16 12:26:12 +0530

    lineextenarccloseview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
@@ -12,13 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsproject8.ui.theme.KotlinGraphicsProject8Theme
-import com.example.lineextendleftarcview.LineExtendLeftArcView
+import com.example.lineextenarccloseview.LineExtendArcCloseView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        LineExtendLeftArcView.create(this)
+        LineExtendArcCloseView.create(this)
     }
 }
 
