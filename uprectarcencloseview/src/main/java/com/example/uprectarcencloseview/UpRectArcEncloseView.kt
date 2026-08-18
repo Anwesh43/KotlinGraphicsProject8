6fd5e5e1802a801842e52776074c9264ffde43ff
Anwesh43 <anweshthecool0@gmail.com>
2026-08-18 12:34:31 +0530

    uprectarcencloseview: adding static create method

--- uprectarcencloseview/src/main/java/com/example/uprectarcencloseview/UpRectArcEncloseView.kt
+++ uprectarcencloseview/src/main/java/com/example/uprectarcencloseview/UpRectArcEncloseView.kt
@@ -214,4 +214,12 @@ class UpRectArcEncloseView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity : Activity) : UpRectArcEncloseView {
+            val view : UpRectArcEncloseView = UpRectArcEncloseView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
