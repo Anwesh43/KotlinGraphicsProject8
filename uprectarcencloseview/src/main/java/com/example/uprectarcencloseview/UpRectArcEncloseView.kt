f640d97e1fbf9a780c2841e132f6e6664010be7a
Anwesh43 <anweshthecool0@gmail.com>
2026-08-18 12:26:54 +0530

    uprectarcencloseview: adding URAENode class

--- uprectarcencloseview/src/main/java/com/example/uprectarcencloseview/UpRectArcEncloseView.kt
+++ uprectarcencloseview/src/main/java/com/example/uprectarcencloseview/UpRectArcEncloseView.kt
@@ -125,4 +125,45 @@ class UpRectArcEncloseView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class URAENode(var i : Int = 0, val state : State = State()) {
+
+        private var next : URAENode? = null
+        private var prev : URAENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = URAENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawURAENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : URAENode {
+            var curr : URAENode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
