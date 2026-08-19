6ebcc54c2e4d68c77cba7c068ed9d2dda32e47ec
Anwesh43 <anweshthecool0@gmail.com>
2026-08-19 13:07:15 +0530

    bentuparcjoinview: adding Canvas extension methods

--- bentuparcjoinview/src/main/java/com/example/bentuparcjoinview/BentUpArcJoinView.kt
+++ bentuparcjoinview/src/main/java/com/example/bentuparcjoinview/BentUpArcJoinView.kt
@@ -28,3 +28,36 @@ val bentDeg : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n))
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBentUpArcJoin(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(-w * 0.5f * (1 - dsc(0)), 0f) {
+                rotate(bentDeg * (1 - j) * dsc(3) + rot * dsc(1) * j)
+                drawLine(0f, 0f, 0f, -size, paint)
+            }
+        }
+        drawArc(RectF(-size, -size, size, size), 180f + bentDeg * dsc(3), rot * dsc(2) - bentDeg * dsc(3), false, paint)
+    }
+}
+
+fun Canvas.drawBUAJNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawBentUpArcJoin(scale, w, h, paint)
+}
