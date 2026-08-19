dd97c6c978046cc8cc7a4580d5ff4a6b11bbb905
Anwesh43 <anweshthecool0@gmail.com>
2026-08-19 13:08:28 +0530

    bentuparcjoinview: adding BentUpArcJoinView clas

--- bentuparcjoinview/src/main/java/com/example/bentuparcjoinview/BentUpArcJoinView.kt
+++ bentuparcjoinview/src/main/java/com/example/bentuparcjoinview/BentUpArcJoinView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawBUAJNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBentUpArcJoin(scale, w, h, paint)
 }
+
+class BentUpArcJoinView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvs : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
