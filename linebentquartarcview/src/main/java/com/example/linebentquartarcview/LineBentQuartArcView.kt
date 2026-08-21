fac965fb272e0cc649e66878686edd23fa506066
Anwesh43 <anweshthecool0@gmail.com>
2026-08-21 15:57:33 +0530

    linebentquartarcview: adding LineBentQuartArcView class

--- linebentquartarcview/src/main/java/com/example/linebentquartarcview/LineBentQuartArcView.kt
+++ linebentquartarcview/src/main/java/com/example/linebentquartarcview/LineBentQuartArcView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawLBQANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineBentQuartArc(scale, w, h, paint)
 }
+
+class LineBentQuartArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
