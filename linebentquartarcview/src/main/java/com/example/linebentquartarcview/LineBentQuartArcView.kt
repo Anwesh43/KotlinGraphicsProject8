0bcea5373afdf21192f83309c50b98a99aa392a3
Anwesh43 <anweshthecool0@gmail.com>
2026-08-21 16:12:49 +0530

    linebentquartarcview: adding Renderer class

--- linebentquartarcview/src/main/java/com/example/linebentquartarcview/LineBentQuartArcView.kt
+++ linebentquartarcview/src/main/java/com/example/linebentquartarcview/LineBentQuartArcView.kt
@@ -190,4 +190,27 @@ class LineBentQuartArcView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineBentQuartArcView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val lbqa : LineBentQuartArc = LineBentQuartArc(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lbqa.draw(canvas, paint)
+            animator.animate {
+                lbqa.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lbqa.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
