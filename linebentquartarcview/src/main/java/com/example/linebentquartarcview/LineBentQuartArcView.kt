73317cc0135c4f04944923517307470e1fe94836
Anwesh43 <anweshthecool0@gmail.com>
2026-08-21 16:07:36 +0530

    linebentquartarcview: adding LBQANode class

--- linebentquartarcview/src/main/java/com/example/linebentquartarcview/LineBentQuartArcView.kt
+++ linebentquartarcview/src/main/java/com/example/linebentquartarcview/LineBentQuartArcView.kt
@@ -126,4 +126,45 @@ class LineBentQuartArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBQANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBQANode? = null
+        private var prev : LBQANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBQANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBQANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBQANode {
+            var curr : LBQANode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
