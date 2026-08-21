ad2b4074542f502d0d595e0097df5d15bf9161c3
Anwesh43 <anweshthecool0@gmail.com>
2026-08-21 15:55:18 +0530

    linebentquartarcview: adding Canvas extension methods

--- linebentquartarcview/src/main/java/com/example/linebentquartarcview/LineBentQuartArcView.kt
+++ linebentquartarcview/src/main/java/com/example/linebentquartarcview/LineBentQuartArcView.kt
@@ -27,3 +27,38 @@ val rot : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBentQuartArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(5), h / 2) {
+        drawLine(0f, 0f, size * 0.5f * dsc(0), 0f, paint)
+        drawXY(size / 2, 0f) {
+            drawXY(0f, 0f) {
+                rotate(-rot * (1 - dsc(3)))
+                drawLine(0f, 0f, size * 0.5f * dsc(1), 0f, paint)
+                drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), -rot, rot * (dsc(2) + dsc(4)), false, paint)
+            }
+
+        }
+    }
+}
+
+fun Canvas.drawLBQANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineBentQuartArc(scale, w, h, paint)
+}
