df9fdd9f779e3a42f4682bcf12463ba7d1b52f54
Anwesh43 <anweshthecool0@gmail.com>
2026-08-21 16:11:32 +0530

    linebentquartarcview: adding LineBentQuartArc class

--- linebentquartarcview/src/main/java/com/example/linebentquartarcview/LineBentQuartArcView.kt
+++ linebentquartarcview/src/main/java/com/example/linebentquartarcview/LineBentQuartArcView.kt
@@ -167,4 +167,27 @@ class LineBentQuartArcView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineBentQuartArc(var i : Int) {
+
+        private var curr : LBQANode = LBQANode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
