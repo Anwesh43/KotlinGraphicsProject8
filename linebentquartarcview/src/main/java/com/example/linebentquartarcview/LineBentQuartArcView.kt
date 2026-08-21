576ea686ea958abba77fa37cb4b2411762b3d766
Anwesh43 <anweshthecool0@gmail.com>
2026-08-21 16:04:02 +0530

    linebentquartarcview: adding Animator class

--- linebentquartarcview/src/main/java/com/example/linebentquartarcview/LineBentQuartArcView.kt
+++ linebentquartarcview/src/main/java/com/example/linebentquartarcview/LineBentQuartArcView.kt
@@ -1,5 +1,6 @@
 package com.example.linebentquartarcview
 
+import android.R
 import android.view.View
 import android.view.MotionEvent
 import android.graphics.Paint
@@ -94,6 +95,34 @@ class LineBentQuartArcView(ctx : Context) : View(ctx) {
             if (dir === 0f) {
                 dir = 1f - 2 * prevScale
                 cb()
+            }
+        }
+    }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
             }
         }
     }
