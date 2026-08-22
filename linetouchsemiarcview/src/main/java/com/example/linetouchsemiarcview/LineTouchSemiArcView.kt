4d498cd306f38fddbf0e6be384a5250fc54e8155
Anwesh43 <anweshthecool0@gmail.com>
2026-08-22 13:11:47 +0530

    linetouchsemiarcview: adding LineTouchSemiArcView class

--- linetouchsemiarcview/src/main/java/com/example/linetouchsemiarcview/LineTouchSemiArcView.kt
+++ linetouchsemiarcview/src/main/java/com/example/linetouchsemiarcview/LineTouchSemiArcView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLTSANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineTouchSemiArc(scale, w, h, paint)
 }
+
+class LineTouchSemiArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
