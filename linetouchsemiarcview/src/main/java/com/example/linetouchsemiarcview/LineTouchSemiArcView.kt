77ac41547402fad843a3be4fdc1502b57571b412
Anwesh43 <anweshthecool0@gmail.com>
2026-08-22 13:39:46 +0530

    linetouchsemiarcview: adding LTSANode class

--- linetouchsemiarcview/src/main/java/com/example/linetouchsemiarcview/LineTouchSemiArcView.kt
+++ linetouchsemiarcview/src/main/java/com/example/linetouchsemiarcview/LineTouchSemiArcView.kt
@@ -123,4 +123,45 @@ class LineTouchSemiArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LTSANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LTSANode? = null
+        private var prev : LTSANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LTSANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLTSANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LTSANode {
+            var curr : LTSANode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
