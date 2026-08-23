8d469c117a36e05a7c2e738c891554f3594df101
Anwesh43 <anweshthecool0@gmail.com>
2026-08-23 12:06:51 +0530

    catchbentlinearcview: adding Renderer class

--- catchbentlinearcview/src/main/java/com/example/catchbentlinearcview/CatchBentLineArcView.kt
+++ catchbentlinearcview/src/main/java/com/example/catchbentlinearcview/CatchBentLineArcView.kt
@@ -193,4 +193,27 @@ class CatchBentLineArcView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : CatchBentLineArcView) {
+
+        private val animator : Animator = Animator(view)
+        private val cbla : CatchBentLineArc = CatchBentLineArc(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            cbla.draw(canvas, paint)
+            animator.animate {
+                cbla.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            cbla.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
