7fdca89ba3c4db274ed07aa9cf2e2b93a5b63eba
Anwesh43 <anweshthecool0@gmail.com>
2026-08-23 11:57:43 +0530

    catchbentlinearcview: adding Canvas extension methods

--- catchbentlinearcview/src/main/java/com/example/catchbentlinearcview/CatchBentLineArcView.kt
+++ catchbentlinearcview/src/main/java/com/example/catchbentlinearcview/CatchBentLineArcView.kt
@@ -23,7 +23,46 @@ val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
 val rot : Float = 180f
+val bentDeg : Float = -45f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawCatchBentLineArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(5), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(-rot * dsc(4))
+            drawXY(0f, 0f) {
+                rotate(bentDeg * (1 - dsc(1)))
+                drawLine(0f, 0f, size * dsc(0), 0f, paint)
+            }
+            drawArc(RectF(0f, -size / 2, size, size / 2), 0f, rot * dsc(2), false, paint)
+        }
+        drawXY(-size, 0f) {
+            drawArc(RectF(0f, -size / 2, size, size / 2), 0f, rot * dsc(3), false, paint)
+        }
+
+    }
+}
+
+fun Canvas.drawCBLANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawCatchBentLineArc(scale, w, h, paint)
+}
