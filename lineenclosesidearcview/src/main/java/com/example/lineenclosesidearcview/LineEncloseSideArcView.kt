a7e0b449f99601697bc52f137a3d1c43eaaa35ca
Anwesh43 <anweshthecool0@gmail.com>
2026-08-26 12:11:45 +0530

    lineenclosesidearcview: adding LineEncloseSideArcView class

--- lineenclosesidearcview/src/main/java/com/example/lineenclosesidearcview/LineEncloseSideArcView.kt
+++ lineenclosesidearcview/src/main/java/com/example/lineenclosesidearcview/LineEncloseSideArcView.kt
@@ -71,3 +71,19 @@ fun Canvas.drawLESANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineEncloseSideArc(scale, w, h, paint)
 }
+
+class LineEncloseSideArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
