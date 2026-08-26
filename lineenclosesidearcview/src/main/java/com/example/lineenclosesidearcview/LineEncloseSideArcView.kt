3646c410fd5bea75e8a4e4870ca231074f7bdfdb
Anwesh43 <anweshthecool0@gmail.com>
2026-08-26 12:10:50 +0530

    lineenclosesidearcview: adding Canvas extension methods

--- lineenclosesidearcview/src/main/java/com/example/lineenclosesidearcview/LineEncloseSideArcView.kt
+++ lineenclosesidearcview/src/main/java/com/example/lineenclosesidearcview/LineEncloseSideArcView.kt
@@ -27,3 +27,47 @@ val rot : Float = -90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineEncloseSideArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2) {
+        rotate(-rot * dsc(3))
+        drawXY(-size / 2, 0f) {
+            drawLine(0f, 0f, 0f, -size * dsc(0) ,paint)
+            drawXY(0f, -size) {
+                drawArc(
+                    RectF(0f, -size / 2, size, size / 2),
+                    180f - 90f * dsc(1),
+                    90f * dsc(1),
+                    false,
+                    paint
+                )
+            }
+        }
+        drawXY(0f, -size / 2) {
+            drawLine(0f, 0f, 0f, size * 0.5f * dsc(2), paint)
+        }
+
+
+    }
+}
+
+fun Canvas.drawLESANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineEncloseSideArc(scale, w, h, paint)
+}
