e0c37c452d448cc4cdc14a9b5ded99359e934618
Anwesh43 <anweshthecool0@gmail.com>
2026-08-26 16:40:24 +0530

    lineenclosesidearcview: adding fix

--- lineenclosesidearcview/src/main/java/com/example/lineenclosesidearcview/LineEncloseSideArcView.kt
+++ lineenclosesidearcview/src/main/java/com/example/lineenclosesidearcview/LineEncloseSideArcView.kt
@@ -41,7 +41,7 @@ fun Canvas.drawLineEncloseSideArc(scale : Float, w : Float, h : Float, paint : P
         scale.divideScale(it, parts)
     }
     drawXY(w / 2 - (w / 2) * dsc(4), h / 2) {
-        rotate(-rot * dsc(3))
+        rotate(rot * dsc(3))
         drawXY(-size / 2, 0f) {
             drawLine(0f, 0f, 0f, -size * dsc(0) ,paint)
             drawXY(0f, -size) {
