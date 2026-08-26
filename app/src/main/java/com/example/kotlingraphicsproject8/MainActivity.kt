0262f25f07da73abbaf4f27114f2ac0e948a26b6
Anwesh43 <anweshthecool0@gmail.com>
2026-08-26 16:39:02 +0530

    lineenclosesidearcview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
@@ -12,13 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsproject8.ui.theme.KotlinGraphicsProject8Theme
-import com.example.lineuparcleftview.LineUpArcLeftView
+import com.example.lineenclosesidearcview.LineEncloseSideArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        LineUpArcLeftView.create(this)
+        LineEncloseSideArcView.create(this)
     }
 }
 
