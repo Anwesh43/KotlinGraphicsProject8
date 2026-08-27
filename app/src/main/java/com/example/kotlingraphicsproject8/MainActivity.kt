a3ed695efcd71e7ce0c24c51ae7dd3f1b88decdb
Anwesh43 <anweshthecool0@gmail.com>
2026-08-27 16:16:25 +0530

    linebentrotarcview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
@@ -12,13 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsproject8.ui.theme.KotlinGraphicsProject8Theme
-import com.example.lineenclosesidearcview.LineEncloseSideArcView
+import com.example.linebentrotarcview.LineBentRotArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        LineEncloseSideArcView.create(this)
+        LineBentRotArcView.create(this)
     }
 }
 
