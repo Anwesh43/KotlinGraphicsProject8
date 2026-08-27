9cc54df60c497e34f147e928a86acff889f11775
Anwesh43 <anweshthecool0@gmail.com>
2026-08-27 13:12:14 +0530

    linebentrotarcview: adding Canvas extension methods

--- linebentrotarcview/src/main/java/com/example/linebentrotarcview/LineBentRotArcView.kt
+++ linebentrotarcview/src/main/java/com/example/linebentrotarcview/LineBentRotArcView.kt
@@ -23,7 +23,43 @@ val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
 val rot : Float = 180f
+val bentDeg : Float = 60f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBentRotArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(bentDeg)
+            drawLine(0f, 0f, size * dsc(0), 0f, paint)
+        }
+        drawArc(RectF(-size, -size, size, size), -bentDeg, bentDeg * dsc(1), false, paint)
+        drawXY(size, 0f) {
+            rotate(rot * dsc(3))
+            drawLine(0f, 0f, size * dsc(2), 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawLBRANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    paint.strokeCap = Paint.Cap.ROUND
+    drawLineBentRotArc(scale, w, h, paint)
+}
