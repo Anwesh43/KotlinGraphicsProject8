ec79034f0f7bfaa3567581e2c07ae0c41337c9cb
Anwesh43 <anweshthecool0@gmail.com>
2026-08-27 13:24:50 +0530

    linebentrotarcview: adding LineBentRotArcView class

--- linebentrotarcview/src/main/java/com/example/linebentrotarcview/LineBentRotArcView.kt
+++ linebentrotarcview/src/main/java/com/example/linebentrotarcview/LineBentRotArcView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawLBRANode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawLineBentRotArc(scale, w, h, paint)
 }
+
+class LineBentRotArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
