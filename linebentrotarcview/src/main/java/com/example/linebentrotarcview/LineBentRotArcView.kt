8dc5a70dbd7c7ac964c53cab9a2abb1be692e003
Anwesh43 <anweshthecool0@gmail.com>
2026-08-27 16:11:22 +0530

    linebentrotarcview: adding Renderer class

--- linebentrotarcview/src/main/java/com/example/linebentrotarcview/LineBentRotArcView.kt
+++ linebentrotarcview/src/main/java/com/example/linebentrotarcview/LineBentRotArcView.kt
@@ -190,4 +190,27 @@ class LineBentRotArcView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineBentRotArcView) {
+
+        private val animator : Animator = Animator(view)
+        private val lbra : LineBentRotArc = LineBentRotArc(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lbra.draw(canvas, paint)
+            animator.animate {
+                lbra.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lbra.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
