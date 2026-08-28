5e2b6f729f9812d1d460f54f294dc457977f9e32
Anwesh43 <anweshthecool0@gmail.com>
2026-08-28 20:51:44 +0530

    steparchalfleftview: adding SAHLNode class

--- steparchalfleftview/src/main/java/com/example/steparchalfleftview/StepArcHalfLeftView.kt
+++ steparchalfleftview/src/main/java/com/example/steparchalfleftview/StepArcHalfLeftView.kt
@@ -122,4 +122,45 @@ class StepArcHalfLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SAHLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SAHLNode? = null
+        private var prev : SAHLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SAHLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSAHLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit){
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SAHLNode {
+            var curr : SAHLNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
