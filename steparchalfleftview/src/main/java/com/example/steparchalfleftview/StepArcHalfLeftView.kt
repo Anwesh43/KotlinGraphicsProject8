c093fde3d777a6264cc989f646267a64e62ef3c4
Anwesh43 <anweshthecool0@gmail.com>
2026-08-28 20:44:45 +0530

    steparchalfleftview: adding StepArcHalfLeftView class

--- steparchalfleftview/src/main/java/com/example/steparchalfleftview/StepArcHalfLeftView.kt
+++ steparchalfleftview/src/main/java/com/example/steparchalfleftview/StepArcHalfLeftView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawSAHLNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawStepArcHalfLeftArc(scale, w, h, paint)
 }
+
+class StepArcHalfLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
