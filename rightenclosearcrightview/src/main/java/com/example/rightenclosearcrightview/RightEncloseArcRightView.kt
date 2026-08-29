9f6b08ea70de7e9416d1aa236d9dc1da40df5772
Anwesh43 <anweshthecool0@gmail.com>
2026-08-29 10:18:08 +0530

    rightenclosearcrightview: adding REARNode class

--- rightenclosearcrightview/src/main/java/com/example/rightenclosearcrightview/RightEncloseArcRightView.kt
+++ rightenclosearcrightview/src/main/java/com/example/rightenclosearcrightview/RightEncloseArcRightView.kt
@@ -124,4 +124,45 @@ class RightEncloseArcRightView(ctx : Context) :View(ctx) {
             }
         }
     }
+
+    data class REARNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : REARNode? = null
+        private var prev : REARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = REARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawREARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : REARNode {
+            var curr : REARNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
