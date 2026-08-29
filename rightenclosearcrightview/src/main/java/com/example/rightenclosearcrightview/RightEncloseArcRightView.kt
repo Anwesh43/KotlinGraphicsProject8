e31f791b62892e6876cbe279b6cdd13ce3c02e21
Anwesh43 <anweshthecool0@gmail.com>
2026-08-29 10:13:09 +0530

    rightenclosearcrightview: adding RightEncloseArcRightView class

--- rightenclosearcrightview/src/main/java/com/example/rightenclosearcrightview/RightEncloseArcRightView.kt
+++ rightenclosearcrightview/src/main/java/com/example/rightenclosearcrightview/RightEncloseArcRightView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawREARNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawRightEncloseArcRight(scale, w, h, paint)
 }
+
+class RightEncloseArcRightView(ctx : Context) :View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
