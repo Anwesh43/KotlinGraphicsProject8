94cad85ffd3c24e1d655e9c21bfb69e7c5f3df6a
Anwesh43 <anweshthecool0@gmail.com>
2026-08-29 10:09:40 +0530

    rightenclosearcrightview: adding Canvas extension methods

--- rightenclosearcrightview/src/main/java/com/example/rightenclosearcrightview/RightEncloseArcRightView.kt
+++ rightenclosearcrightview/src/main/java/com/example/rightenclosearcrightview/RightEncloseArcRightView.kt
@@ -27,3 +27,37 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRightEncloseArcRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        rotate(rot * dsc(3))
+        drawXY(0f, -size) {
+            drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+            drawXY(-size, 0f) {
+                drawLine(0f, 0f, 0f, size * dsc(1), paint)
+            }
+        }
+        drawArc(RectF(-size, -size, size, size), 180f, 90f * dsc(2), false, paint)
+    }
+}
+
+fun Canvas.drawREARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawRightEncloseArcRight(scale, w, h, paint)
+}
