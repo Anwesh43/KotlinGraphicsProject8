faf95fbe6dbd8f9caaa123117919f9ffefc01ffa
Anwesh43 <anweshthecool0@gmail.com>
2026-08-29 11:02:00 +0530

    rightenclosearcrightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
@@ -12,13 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsproject8.ui.theme.KotlinGraphicsProject8Theme
-import com.example.steparchalfleftview.StepArcHalfLeftView
+import com.example.rightenclosearcrightview.RightEncloseArcRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        StepArcHalfLeftView.create(this)
+        RightEncloseArcRightView.create(this)
     }
 }
 
