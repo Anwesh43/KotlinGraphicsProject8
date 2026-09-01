86e1e2b667ea0e4ad8ebb6c0085098e8a82c1cd6
Anwesh43 <anweshthecool0@gmail.com>
2026-09-01 20:08:40 +0530

    arcsideellipmoveview: adding ASEMNode class

--- arcsideellipmoveview/src/main/java/com/example/arcsideellipmoveview/ArcSideEllipMoveView.kt
+++ arcsideellipmoveview/src/main/java/com/example/arcsideellipmoveview/ArcSideEllipMoveView.kt
@@ -122,4 +122,45 @@ class ArcSideEllipMoveView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ASEMNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ASEMNode? = null
+        private var prev : ASEMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ASEMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawASEMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdatiung(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ASEMNode {
+            var curr : ASEMNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
