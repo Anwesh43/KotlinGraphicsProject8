7a3d61180f858b752b05a47487a1993d74ed8890
Anwesh43 <anweshthecool0@gmail.com>
2026-09-01 19:48:02 +0530

    arcsideellipmoveview: adding ArcSideEllipMoveView class

--- arcsideellipmoveview/src/main/java/com/example/arcsideellipmoveview/ArcSideEllipMoveView.kt
+++ arcsideellipmoveview/src/main/java/com/example/arcsideellipmoveview/ArcSideEllipMoveView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawASEMNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawArcSideEllipMove(scale, w, h, paint)
 }
+
+class ArcSideEllipMoveView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
