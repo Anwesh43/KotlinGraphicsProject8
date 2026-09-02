c56bf0345d2da0d322e8634711814782fd12104a
Anwesh43 <anweshthecool0@gmail.com>
2026-09-02 11:39:53 +0530

    downlinerotarcview: adding DownLineRotArcView class

--- downlinerotarcview/src/main/java/com/example/downlinerotarcview/DownLineRotArcView.kt
+++ downlinerotarcview/src/main/java/com/example/downlinerotarcview/DownLineRotArcView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawDLRANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawDownLineRotArc(scale, w, h, paint)
 }
+
+class DownLineRotArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
