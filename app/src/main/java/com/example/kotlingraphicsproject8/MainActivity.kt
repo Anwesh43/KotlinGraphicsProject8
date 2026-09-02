9e0e418002216e0a41a5cc90ecc8fd6338706593
Anwesh43 <anweshthecool0@gmail.com>
2026-09-02 12:04:11 +0530

    downlinerotarcview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.arcsideellipmoveview.ArcSideEllipMoveView
+import com.example.downlinerotarcview.DownLineRotArcView
 import com.example.kotlingraphicsproject8.ui.theme.KotlinGraphicsProject8Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        ArcSideEllipMoveView.create(this)
+        DownLineRotArcView.create(this)
     }
 }
 
