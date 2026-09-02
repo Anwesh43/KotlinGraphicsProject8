d9871dc4ba725a278999d46f2cda5300c2d98bb3
Anwesh43 <anweshthecool0@gmail.com>
2026-09-02 11:31:23 +0530

    downlinerotarcview: adding Canvas extension methods

--- downlinerotarcview/src/main/java/com/example/downlinerotarcview/DownLineRotArcView.kt
+++ downlinerotarcview/src/main/java/com/example/downlinerotarcview/DownLineRotArcView.kt
@@ -28,3 +28,35 @@ val deg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawDownLineRotArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2) {
+        rotate(deg * dsc(3))
+        drawXY(0f, -h * 0.5f * (1 - dsc(0))) {
+            rotate(rot * dsc(1))
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+        drawArc(RectF(-size / 2, 0f, size /2 , size), 90f, 180f * dsc(2), false, paint)
+    }
+}
+
+fun Canvas.drawDLRANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawDownLineRotArc(scale, w, h, paint)
+}
