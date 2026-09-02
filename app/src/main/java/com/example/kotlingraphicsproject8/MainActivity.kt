0cd121b84ff6e576f19e45fd87ec1887bc34561f
Anwesh43 <anweshthecool0@gmail.com>
2026-09-02 09:15:31 +0530

    arcsideellipmoveview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.arcsideellipmoveview.ArcSideEllipMoveView
 import com.example.kotlingraphicsproject8.ui.theme.KotlinGraphicsProject8Theme
-import com.example.rightenclosearcrightview.RightEncloseArcRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        RightEncloseArcRightView.create(this)
+        ArcSideEllipMoveView.create(this)
     }
 }
 
