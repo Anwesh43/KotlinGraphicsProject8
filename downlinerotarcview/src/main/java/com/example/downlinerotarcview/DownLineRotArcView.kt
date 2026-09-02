8f3ddd26fcead83e50c913c520cd42d791f25910
Anwesh43 <anweshthecool0@gmail.com>
2026-09-02 11:53:27 +0530

    downlinerotarcview: adding DLRANode class

--- downlinerotarcview/src/main/java/com/example/downlinerotarcview/DownLineRotArcView.kt
+++ downlinerotarcview/src/main/java/com/example/downlinerotarcview/DownLineRotArcView.kt
@@ -123,4 +123,45 @@ class DownLineRotArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DLRANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : DLRANode? = null
+        private var prev : DLRANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = DLRANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDLRANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DLRANode {
+            var curr : DLRANode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
