71a47bbca80097cb45e3371cc4abd840e80f2c69
Anwesh43 <anweshthecool0@gmail.com>
2026-09-04 04:51:56 +0530

    lineopenbentrightview: adding LineOpenBentRightView class

--- lineopenbentrightview/src/main/java/com/example/lineopenbentrightview/LineOpenBentRightView.kt
+++ lineopenbentrightview/src/main/java/com/example/lineopenbentrightview/LineOpenBentRightView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawLOBRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineOpenBentRight(scale, w, h, paint)
 }
+
+class LineOpenBentRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
