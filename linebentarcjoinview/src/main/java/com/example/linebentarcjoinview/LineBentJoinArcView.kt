1e9ae2aa0f7c09ca009f5be1c44fa45329b5a0a8
Anwesh43 <anweshthecool0@gmail.com>
2026-09-03 13:22:20 +0530

    linebentarcjoinview: adding Canvas extension methods

--- linebentarcjoinview/src/main/java/com/example/linebentarcjoinview/LineBentJoinArcView.kt
+++ linebentarcjoinview/src/main/java/com/example/linebentarcjoinview/LineBentJoinArcView.kt
@@ -16,14 +16,48 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 5
-val scGap : Float = 0.04f / parts
+val parts : Int = 6
+val scGap : Float = 0.05f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
-val rot : Float = -60f
+val rot : Float = 60f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBentJoinArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        drawXY(-w * 0.5f * (1 - dsc(1)), 0f) {
+            rotate(-rot)
+            drawLine(0f, 0f, size * dsc(0), 0f, paint)
+        }
+        drawArc(RectF(-size, -size, size, size), -rot, rot * dsc(2), false, paint)
+        drawXY(size, 0f) {
+            drawLine(-size * dsc(3), 0f, 0f, 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawLBJANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineBentJoinArc(scale, w, h, paint)
+}
