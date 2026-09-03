264587b5d1878a2b8f84c6ebe3b67e73987baaf1
Anwesh43 <anweshthecool0@gmail.com>
2026-09-03 14:47:14 +0530

    linebentarcjoinview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.downlinerotarcview.DownLineRotArcView
 import com.example.kotlingraphicsproject8.ui.theme.KotlinGraphicsProject8Theme
+import com.example.linebentarcjoinview.LineBentJoinArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        DownLineRotArcView.create(this)
+        LineBentJoinArcView.create(this)
     }
 }
 
