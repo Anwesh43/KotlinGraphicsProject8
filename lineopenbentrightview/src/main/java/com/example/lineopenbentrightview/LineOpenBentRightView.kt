77678df9270dd88d35d67e56bfc06c62d488ae6c
Anwesh43 <anweshthecool0@gmail.com>
2026-09-04 04:50:42 +0530

    lineopenbentrightview: adding Canvas extension methods

--- lineopenbentrightview/src/main/java/com/example/lineopenbentrightview/LineOpenBentRightView.kt
+++ lineopenbentrightview/src/main/java/com/example/lineopenbentrightview/LineOpenBentRightView.kt
@@ -16,15 +16,49 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 5
-val scGap : Float = 0.04f / parts
+val parts : Int = 6
+val scGap : Float = 0.05f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
 val rot : Float = 90f
+val bentDeg : Float = 45f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineOpenBentRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(5), h / 2) {
+        rotate(rot * dsc(4))
+        for (j in 0..1) {
+            drawXY(0f, -h * 0.5f * (1 - dsc(j * 2))) {
+                rotate(-bentDeg * (1 - j) * dsc(1))
+                drawLine(0f, 0f, 0f, -size, paint)
+            }
+        }
+        drawArc(RectF(-size, -size, size, size), -rot -bentDeg, bentDeg * dsc(3), false, paint)
+    }
+}
+
+fun Canvas.drawLOBRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineOpenBentRight(scale, w, h, paint)
+}
