fdd0eb3f00a494d432f2958b966b41d72839dc9e
Anwesh43 <anweshthecool0@gmail.com>
2026-09-04 08:57:37 +0530

    lineopenbentrightview: adding LineOpenBentRight class

--- lineopenbentrightview/src/main/java/com/example/lineopenbentrightview/LineOpenBentRightView.kt
+++ lineopenbentrightview/src/main/java/com/example/lineopenbentrightview/LineOpenBentRightView.kt
@@ -166,4 +166,27 @@ class LineOpenBentRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineOpenBentRight(var i : Int) {
+
+        private var curr : LOBRNode = LOBRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
