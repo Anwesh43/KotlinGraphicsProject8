8b277a42ac9d8147e839a08e3e7018567e642b2a
Anwesh43 <anweshthecool0@gmail.com>
2026-09-04 09:12:06 +0530

    lineopenbentrightview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
@@ -12,13 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsproject8.ui.theme.KotlinGraphicsProject8Theme
-import com.example.linebentarcjoinview.LineBentJoinArcView
+import com.example.lineopenbentrightview.LineOpenBentRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        LineBentJoinArcView.create(this)
+        LineOpenBentRightView.create(this)
     }
 }
 
