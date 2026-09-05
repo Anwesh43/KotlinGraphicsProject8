419c93b17b52df98ff53a07178c4d03617beef88
Anwesh43 <anweshthecool0@gmail.com>
2026-09-05 07:01:08 +0530

    linemovesweepupview: adding LMSUNode class

--- linemovesweepupview/src/main/java/com/example/linemovesweepupview/LineMoveSweepUpView.kt
+++ linemovesweepupview/src/main/java/com/example/linemovesweepupview/LineMoveSweepUpView.kt
@@ -123,4 +123,45 @@ class LineMoveSweepUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LMSUNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LMSUNode? = null
+        private var prev : LMSUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LMSUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLMSUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LMSUNode {
+            var curr : LMSUNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
