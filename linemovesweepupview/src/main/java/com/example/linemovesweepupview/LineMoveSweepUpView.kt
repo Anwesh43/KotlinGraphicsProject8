f076aa0c8ff3c64b12e8bcc7acba8f5da6fa14df
Anwesh43 <anweshthecool0@gmail.com>
2026-09-05 06:47:14 +0530

    linemovesweepupview: adding Canvas extension methods

--- linemovesweepupview/src/main/java/com/example/linemovesweepupview/LineMoveSweepUpView.kt
+++ linemovesweepupview/src/main/java/com/example/linemovesweepupview/LineMoveSweepUpView.kt
@@ -22,7 +22,41 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val rot : Float = 70f
+val deg : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineMoveSweepUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w * 0.5f * dsc(0), h * 0.5f * (1 - dsc(3))) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(deg * dsc(1) + rot * j * dsc(2))
+                drawLine(0f, 0f, 0f, -size, paint)
+            }
+        }
+        drawArc(RectF(-size, -size, size, size), -90f, rot * dsc(2), false, paint)
+    }
+}
+
+fun Canvas.drawLMSUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineMoveSweepUp(scale, w, h, paint)
+}
