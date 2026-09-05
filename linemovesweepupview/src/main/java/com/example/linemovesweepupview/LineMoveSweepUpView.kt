0e5f3e36342e97bbe273f89ba7bbf29b91373ae1
Anwesh43 <anweshthecool0@gmail.com>
2026-09-05 07:07:42 +0530

    linemovesweepupview: adding Renderer class

--- linemovesweepupview/src/main/java/com/example/linemovesweepupview/LineMoveSweepUpView.kt
+++ linemovesweepupview/src/main/java/com/example/linemovesweepupview/LineMoveSweepUpView.kt
@@ -23,6 +23,7 @@ val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val rot : Float = 70f
 val deg : Float = 90f
+val backColor : Int = "#BDBDBD".toColorInt()
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -185,6 +186,29 @@ class LineMoveSweepUpView(ctx : Context) : View(ctx) {
 
         fun startUpdating(cb : () -> Unit) {
             curr.startUpdating(cb)
+        }
+    }
+
+    data class Renderer(var view : LineMoveSweepUpView) {
+
+        private val animator : Animator = Animator(view)
+        private val lmsu : LineMoveSweepUp = LineMoveSweepUp(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lmsu.draw(canvas, paint)
+            animator.animate {
+                lmsu.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lmsu.startUpdating {
+                animator.start()
+            }
         }
     }
 }
