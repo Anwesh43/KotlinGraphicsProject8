86a3cda382939c775086d2cbfe6b602d4c716a78
Anwesh43 <anweshthecool0@gmail.com>
2026-09-06 11:23:38 +0530

    lineopenarcrightview: adding view in activit

--- app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
@@ -12,13 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsproject8.ui.theme.KotlinGraphicsProject8Theme
-import com.example.linemovesweepupview.LineMoveSweepUpView
+import com.example.lineopenarcrightview.LineOpenArcRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        LineMoveSweepUpView.create(this)
+        LineOpenArcRightView.create(this)
     }
 }
 
