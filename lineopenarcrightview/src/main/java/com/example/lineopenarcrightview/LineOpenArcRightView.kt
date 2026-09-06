742b73ee4ff0ed5e0fb9cd5e354c42f94b4ee312
Anwesh43 <anweshthecool0@gmail.com>
2026-09-06 10:24:35 +0530

    lineopenarcrightview: adding LineOpenArcRightView class

--- lineopenarcrightview/src/main/java/com/example/lineopenarcrightview/LineOpenArcRightView.kt
+++ lineopenarcrightview/src/main/java/com/example/lineopenarcrightview/LineOpenArcRightView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawLOARNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineOpenArcRight(scale, w, h, paint)
 }
+
+class LineOpenArcRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
