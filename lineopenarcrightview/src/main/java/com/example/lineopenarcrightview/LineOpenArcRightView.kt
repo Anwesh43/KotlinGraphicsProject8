71ede045aead5e23fa6e068d69fe18bc14036232
Anwesh43 <anweshthecool0@gmail.com>
2026-09-06 10:43:56 +0530

    lineopenarcrightview: adding LOARNode class

--- lineopenarcrightview/src/main/java/com/example/lineopenarcrightview/LineOpenArcRightView.kt
+++ lineopenarcrightview/src/main/java/com/example/lineopenarcrightview/LineOpenArcRightView.kt
@@ -126,4 +126,45 @@ class LineOpenArcRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LOARNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LOARNode? = null
+        private var prev : LOARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LOARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLOARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LOARNode {
+            var curr : LOARNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
