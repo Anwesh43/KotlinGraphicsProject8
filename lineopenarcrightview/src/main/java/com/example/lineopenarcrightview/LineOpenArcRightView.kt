3f05feb5b1789caa86af944afffadd923f499fd6
Anwesh43 <anweshthecool0@gmail.com>
2026-09-06 10:48:48 +0530

    lineopenarcrightview: adding Renderer in View

--- lineopenarcrightview/src/main/java/com/example/lineopenarcrightview/LineOpenArcRightView.kt
+++ lineopenarcrightview/src/main/java/com/example/lineopenarcrightview/LineOpenArcRightView.kt
@@ -66,14 +66,16 @@ fun Canvas.drawLOARNode(i : Int, scale : Float, paint : Paint) {
 
 class LineOpenArcRightView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
