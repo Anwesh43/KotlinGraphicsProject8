2f0446eda03b763fec155345783267a8b3a0564e
Anwesh43 <anweshthecool0@gmail.com>
2026-09-07 09:55:30 +0530

    midbentlinearcview: adding Canvas extension methods

--- midbentlinearcview/src/main/java/com/example/midbentlinearcview/MidBentLineArcView.kt
+++ midbentlinearcview/src/main/java/com/example/midbentlinearcview/MidBentLineArcView.kt
@@ -22,8 +22,40 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
-val rot : Float = 60f
+val rot : Float = 30f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawMidBentLineArc(scale : Float, w  : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        drawXY(-w * 0.25f * (1 - dsc(2)),  -h * 0.25f * (1 - dsc(0))) {
+            rotate(rot * dsc(1))
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+        val sweepDeg : Float = 90f - rot
+        drawArc(RectF(-size, -size, size, size), -sweepDeg, sweepDeg * dsc(3), false, paint)
+    }
+}
+
+fun Canvas.drawMBLANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawMidBentLineArc(scale, w, h, paint)
+}
