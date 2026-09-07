f3880f7633e769edddfeb27ed9a752bf964afa45
Anwesh43 <anweshthecool0@gmail.com>
2026-09-07 10:21:35 +0530

    midbentlinearcview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
@@ -12,13 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsproject8.ui.theme.KotlinGraphicsProject8Theme
-import com.example.lineopenarcrightview.LineOpenArcRightView
+import com.example.midbentlinearcview.MidBentLineArcView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        LineOpenArcRightView.create(this)
+        MidBentLineArcView.create(this)
     }
 }
 
