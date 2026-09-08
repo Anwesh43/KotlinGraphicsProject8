5b695ab3f149d25cd155fc5833a7c337681a83b4
Anwesh43 <anweshthecool0@gmail.com>
2026-09-08 10:22:15 +0530

    semiarccompletehalfview: adding Renderer class

--- semiarccompletehalfview/src/main/java/com/example/semiarccompletehalfview/SemiArcCompleteHalfView.kt
+++ semiarccompletehalfview/src/main/java/com/example/semiarccompletehalfview/SemiArcCompleteHalfView.kt
@@ -185,4 +185,27 @@ class SemiArcCompleteHalfView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SemiArcCompleteHalfView) {
+
+        private val sach : SemiArcCompleteHalf = SemiArcCompleteHalf(0)
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            sach.draw(canvas, paint)
+            animator.animate {
+                sach.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            sach.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
