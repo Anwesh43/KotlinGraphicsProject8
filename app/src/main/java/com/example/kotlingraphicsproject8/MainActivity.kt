8ccfa82f40bbb9b6ec36ecbe363366ddc9d252aa
Anwesh43 <anweshthecool0@gmail.com>
2026-09-08 10:25:18 +0530

    semiarccompletehalfview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
@@ -12,13 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsproject8.ui.theme.KotlinGraphicsProject8Theme
-import com.example.midbentlinearcview.MidBentLineArcView
+import com.example.semiarccompletehalfview.SemiArcCompleteHalfView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        MidBentLineArcView.create(this)
+        SemiArcCompleteHalfView.create(this)
     }
 }
 
