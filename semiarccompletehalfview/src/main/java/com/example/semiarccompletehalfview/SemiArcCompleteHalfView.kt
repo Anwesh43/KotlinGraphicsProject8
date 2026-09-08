9e95f4d6b10c8c23c29c65a966d34128b6e27898
Anwesh43 <anweshthecool0@gmail.com>
2026-09-08 09:43:49 +0530

    semiarccompletehalfview: adding Canvas extension methods

--- semiarccompletehalfview/src/main/java/com/example/semiarccompletehalfview/SemiArcCompleteHalfView.kt
+++ semiarccompletehalfview/src/main/java/com/example/semiarccompletehalfview/SemiArcCompleteHalfView.kt
@@ -27,3 +27,34 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSemiArcCompleteHalf(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        drawXY(0f, -h * 0.25f * (1 - dsc(1))) {
+            rotate(rot * dsc(2))
+            drawArc(RectF(-size / 2, -size, size / 2, 0f), -90f, 180f * dsc(0), false, paint)
+        }
+        drawArc(RectF(0f, -size / 2, size, size / 2), 0f, 180f * dsc(3), false, paint)
+    }
+}
+
+fun Canvas.drawSACHNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawSemiArcCompleteHalf(scale, w, h, paint)
+}
