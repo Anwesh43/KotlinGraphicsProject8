b3856baef81d7294f9cb1ca2844d189fd2abfb71
Anwesh43 <anweshthecool0@gmail.com>
2026-09-08 10:23:45 +0530

    semiarccompletehalfview: adding static create method

--- semiarccompletehalfview/src/main/java/com/example/semiarccompletehalfview/SemiArcCompleteHalfView.kt
+++ semiarccompletehalfview/src/main/java/com/example/semiarccompletehalfview/SemiArcCompleteHalfView.kt
@@ -210,4 +210,13 @@ class SemiArcCompleteHalfView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity: Activity) : SemiArcCompleteHalfView {
+            val view : SemiArcCompleteHalfView = SemiArcCompleteHalfView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
