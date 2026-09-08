d0bddec9a4bea4d6d8acaa1807995d5a19c2fb48
Anwesh43 <anweshthecool0@gmail.com>
2026-09-08 09:57:46 +0530

    semiarccompletehalfview: adding SemiArcCompleteHalf clas

--- semiarccompletehalfview/src/main/java/com/example/semiarccompletehalfview/SemiArcCompleteHalfView.kt
+++ semiarccompletehalfview/src/main/java/com/example/semiarccompletehalfview/SemiArcCompleteHalfView.kt
@@ -162,4 +162,27 @@ class SemiArcCompleteHalfView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SemiArcCompleteHalf(var i : Int) {
+
+        private var curr : SACHNode = SACHNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir  *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
