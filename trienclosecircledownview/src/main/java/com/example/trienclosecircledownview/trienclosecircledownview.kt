f877ee63e315322f6caf933ae925edeb0ae127f3
Anwesh43 <anweshthecool0@gmail.com>
2026-09-09 10:44:52 +0530

    trienclosecircledownview: adding TECDNode class

--- trienclosecircledownview/src/main/java/com/example/trienclosecircledownview/trienclosecircledownview.kt
+++ trienclosecircledownview/src/main/java/com/example/trienclosecircledownview/trienclosecircledownview.kt
@@ -122,4 +122,45 @@ class TriEncloseCircleDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TECDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : TECDNode? = null
+        private var prev : TECDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TECDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTECDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TECDNode {
+            var curr : TECDNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
