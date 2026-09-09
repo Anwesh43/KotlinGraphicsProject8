f35a3f7a9877c6bda456ed73c7a6955c54abddba
Anwesh43 <anweshthecool0@gmail.com>
2026-09-09 11:38:36 +0530

    trienclosecircledownview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
@@ -12,13 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlingraphicsproject8.ui.theme.KotlinGraphicsProject8Theme
-import com.example.semiarccompletehalfview.SemiArcCompleteHalfView
+import com.example.trienclosecircledownview.TriEncloseCircleDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        SemiArcCompleteHalfView.create(this)
+        TriEncloseCircleDownView.create(this)
     }
 }
 
