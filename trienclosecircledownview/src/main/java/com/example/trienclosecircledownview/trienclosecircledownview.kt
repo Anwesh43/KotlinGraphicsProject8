e7214fe4eab96b535327ddf0bbf707868156489e
Anwesh43 <anweshthecool0@gmail.com>
2026-09-09 09:57:51 +0530

    trienclosecircledownview: adding constants and libraries

---
+++ trienclosecircledownview/src/main/java/com/example/trienclosecircledownview/trienclosecircledownview.kt
@@ -0,0 +1,25 @@
+package com.example.trienclosecircledownview
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Canvas
+import androidx.core.graphics.toColorInt
+
+val colors : Array<String> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+)
+val parts : Int = 5
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 5.9f
+val delay : Long = 20
+val backColor : Int = "#BDBDBD".toColorInt()
+val rot : Float = 90f
