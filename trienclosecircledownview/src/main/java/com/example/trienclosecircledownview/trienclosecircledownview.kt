d491ed8915f845ecd1e755801ea5d908441ed37d
Anwesh43 <anweshthecool0@gmail.com>
2026-09-09 11:31:57 +0530

    trienclosecircledownview: adding TriEncloseCircleDown class

--- trienclosecircledownview/src/main/java/com/example/trienclosecircledownview/trienclosecircledownview.kt
+++ trienclosecircledownview/src/main/java/com/example/trienclosecircledownview/trienclosecircledownview.kt
@@ -163,4 +163,28 @@ class TriEncloseCircleDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class TriEncloseCircleDown(var i : Int) {
+
+        private var curr : TECDNode = TECDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+
+    }
 }
