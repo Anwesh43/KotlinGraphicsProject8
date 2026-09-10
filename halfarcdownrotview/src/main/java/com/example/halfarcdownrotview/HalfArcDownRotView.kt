f5d64673c07db153475d71202774d5d5b85f536e
Anwesh43 <anweshthecool0@gmail.com>
2026-09-10 10:56:48 +0530

    halfarcdownrotview: adding HalfArcDownRotView class

--- halfarcdownrotview/src/main/java/com/example/halfarcdownrotview/HalfArcDownRotView.kt
+++ halfarcdownrotview/src/main/java/com/example/halfarcdownrotview/HalfArcDownRotView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawHADRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawHalfArcDownRot(scale, w, h, paint)
 }
+
+class HalfArcDownRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
