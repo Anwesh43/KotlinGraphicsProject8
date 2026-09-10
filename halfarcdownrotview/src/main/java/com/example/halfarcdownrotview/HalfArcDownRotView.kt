d24ba1d36b765b36ff0e8acc10353065bfdeed71
Anwesh43 <anweshthecool0@gmail.com>
2026-09-10 10:49:36 +0530

    halfarcdownrotview: adding Canvas extension methods

--- halfarcdownrotview/src/main/java/com/example/halfarcdownrotview/HalfArcDownRotView.kt
+++ halfarcdownrotview/src/main/java/com/example/halfarcdownrotview/HalfArcDownRotView.kt
@@ -27,3 +27,37 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawHalfArcDownRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(5), h * 0.5f * dsc(1)) {
+        rotate(rot * dsc(2))
+        drawArc(RectF(-size, -size, size, size), 0f, 180f * dsc(0), false, paint)
+        drawXY(size, 0f) {
+            drawLine(0f, 0f, -size * dsc(3), size * dsc(3), paint)
+        }
+        drawXY(0f, size) {
+            drawLine(0f, 0f, -size * dsc(4), -size * dsc(4), paint)
+        }
+    }
+}
+
+fun Canvas.drawHADRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawHalfArcDownRot(scale, w, h, paint)
+}
