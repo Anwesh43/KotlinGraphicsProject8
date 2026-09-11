c8b389ac2e4bd88a1eb147c7368f1d581865f848
Anwesh43 <anweshthecool0@gmail.com>
2026-09-11 10:11:55 +0530

    linebiconcarcview: adding Canvas extension methods

--- linebiconcarcview/src/main/java/com/example/linebiconcarcview/LineBiConcArcView.kt
+++ linebiconcarcview/src/main/java/com/example/linebiconcarcview/LineBiConcArcView.kt
@@ -27,3 +27,35 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBiConcArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(4)) {
+        rotate(rot * dsc(3))
+        drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        for (j in 0..1) {
+            val r : Float = size / (1 + j)
+            drawArc(RectF(-r, -r, r, r), -90f, 90f * dsc(1 + j), false, paint)
+        }
+    }
+}
+
+fun Canvas.drawLBCANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineBiConcArc(scale, w, h, paint)
+}
