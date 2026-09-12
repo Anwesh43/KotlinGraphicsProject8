e7488d975a846390a76d6aab70bb6e357261b026
Anwesh43 <anweshthecool0@gmail.com>
2026-09-12 10:46:07 +0530

    bowarrowownleftview: adding Canvas extension methods

--- bowarrowdownleftview/src/main/java/com/example/bowarrowdownleftview/BowArrowDownLeftView.kt
+++ bowarrowdownleftview/src/main/java/com/example/bowarrowdownleftview/BowArrowDownLeftView.kt
@@ -16,8 +16,8 @@ val colors : Array<String> = arrayOf(
     "#C51162",
     "#00C853"
 )
-val parts : Int = 5
-val scGap : Float = 0.04f / parts
+val parts : Int = 6
+val scGap : Float = 0.05f / parts
 val rot : Float = 90f
 val sizeFactor : Float = 5.9f
 val delay : Long = 20
@@ -27,5 +27,36 @@ val strokeFactor : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBowArrowDownLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(5), h / 2) {
+        rotate(rot * dsc(4))
+        drawArc(RectF(-size, -size, size, size), 0f, rot * (dsc(0) + dsc(2)), false, paint)
+        drawXY(0f, size + (h / 2) * dsc(3)) {
+            drawLine(0f, 0f, 0f, size * dsc(1), paint)
+        }
+    }
+}
+
+fun Canvas.drawBADLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawBowArrowDownLeft(scale, w, h, paint)
+}
 
 
