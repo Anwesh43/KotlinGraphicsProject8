2365ac6fd0bbf2b918dfc7d36c553b7efbf76656
Anwesh43 <anweshthecool0@gmail.com>
2026-09-13 12:23:45 +0530

    uplinearcrightview: adding Canvas extension methods

--- uplinearcrightview/src/main/java/com/example/uplinearcrightview/UpLineArcRightView.kt
+++ uplinearcrightview/src/main/java/com/example/uplinearcrightview/UpLineArcRightView.kt
@@ -27,3 +27,37 @@ val sizeFactor : Float = 5.9f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawUpLineArcRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(-rot * dsc(2))
+            drawArc(RectF(-size / 2, 0f, size / 2, size), 90f, 180f * dsc(0), false, paint)
+        }
+        drawXY(0f, -h * 0.5f * (1 - dsc(1))) {
+            rotate(rot * dsc(3))
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+    }
+}
+
+fun Canvas.drawULARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawUpLineArcRight(scale, w, h, paint)
+}
