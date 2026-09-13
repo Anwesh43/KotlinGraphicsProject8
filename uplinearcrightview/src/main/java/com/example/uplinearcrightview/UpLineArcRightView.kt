fea4c1e26a20dbe908dd3450689edd4b3da2f3e6
Anwesh43 <anweshthecool0@gmail.com>
2026-09-13 12:43:04 +0530

    uplinearcrightview: adding ULARNode class

--- uplinearcrightview/src/main/java/com/example/uplinearcrightview/UpLineArcRightView.kt
+++ uplinearcrightview/src/main/java/com/example/uplinearcrightview/UpLineArcRightView.kt
@@ -124,4 +124,45 @@ class UpLineArcRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ULARNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ULARNode? = null
+        private var prev : ULARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ULARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawULARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ULARNode {
+            var curr : ULARNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
