eebd88ab1c2007007731ecbb634b9b71f7d2bd8f
Anwesh43 <anweshthecool0@gmail.com>
2026-09-13 12:25:44 +0530

    uplinearcrightview: adding UpLineArcRightView class

--- uplinearcrightview/src/main/java/com/example/uplinearcrightview/UpLineArcRightView.kt
+++ uplinearcrightview/src/main/java/com/example/uplinearcrightview/UpLineArcRightView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawULARNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawUpLineArcRight(scale, w, h, paint)
 }
+
+class UpLineArcRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
