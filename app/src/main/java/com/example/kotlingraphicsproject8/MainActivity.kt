acf21eeeffb3feec0e02c756846eb357f148e78d
Anwesh43 <anweshthecool0@gmail.com>
2026-09-14 09:44:56 +0530

    halfarcjoinview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.halfarcmidjoinview.HalfArcJoinMidView
 import com.example.kotlingraphicsproject8.ui.theme.KotlinGraphicsProject8Theme
-import com.example.uplinearcrightview.UpLineArcRightView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        UpLineArcRightView.create(this)
+        HalfArcJoinMidView.create(this)
     }
 }
 
