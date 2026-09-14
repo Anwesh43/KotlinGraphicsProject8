bd22e86382d6c85d00582d6d7519c2a4756c0941
Anwesh43 <anweshthecool0@gmail.com>
2026-09-14 09:24:48 +0530

    halfarcjoinview: adding HalfArcJoinMidView class

--- halfarcmidjoinview/src/main/java/com/example/halfarcmidjoinview/HalfArcJoinMidView.kt
+++ halfarcmidjoinview/src/main/java/com/example/halfarcmidjoinview/HalfArcJoinMidView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawHAJMNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawHalfArcJoinMid(scale, w, h, paint)
 }
+
+class HalfArcJoinMidView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
