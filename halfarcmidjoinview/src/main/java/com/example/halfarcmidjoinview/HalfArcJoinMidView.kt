81b8e3f19a30481c7debd5536144d8a9029f64b9
Anwesh43 <anweshthecool0@gmail.com>
2026-09-14 09:35:29 +0530

    halfarcjoinview: adding HAJMNode class

--- halfarcmidjoinview/src/main/java/com/example/halfarcmidjoinview/HalfArcJoinMidView.kt
+++ halfarcmidjoinview/src/main/java/com/example/halfarcmidjoinview/HalfArcJoinMidView.kt
@@ -125,4 +125,45 @@ class HalfArcJoinMidView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HAJMNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : HAJMNode? = null
+        private var prev : HAJMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HAJMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHAJMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HAJMNode {
+            var curr : HAJMNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
