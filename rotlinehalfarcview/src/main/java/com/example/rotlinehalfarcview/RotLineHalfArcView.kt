08099f4825ffc220a04a5a8642e6e647d71e1564
Anwesh43 <anweshthecool0@gmail.com>
2026-09-15 09:34:02 +0530

    rotlinehalfarcview : adding RotLineHalfArcView class

--- rotlinehalfarcview/src/main/java/com/example/rotlinehalfarcview/RotLineHalfArcView.kt
+++ rotlinehalfarcview/src/main/java/com/example/rotlinehalfarcview/RotLineHalfArcView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawRLHANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawRotLineHalfArc(scale, w, h, paint)
 }
+
+class RotLineHalfArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
