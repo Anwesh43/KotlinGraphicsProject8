4fae8a61a6d9763271d5d486451e7e1b5511410e
Anwesh43 <anweshthecool0@gmail.com>
2026-09-15 09:33:06 +0530

    rotlinehalfarcview : adding Canvas extension methods

--- rotlinehalfarcview/src/main/java/com/example/rotlinehalfarcview/RotLineHalfArcView.kt
+++ rotlinehalfarcview/src/main/java/com/example/rotlinehalfarcview/RotLineHalfArcView.kt
@@ -28,3 +28,38 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRotLineHalfArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2) * dsc(4), h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY(size * 0.25f * (1 - dsc(2)), 0f) {
+                    rotate(rot * dsc(1) * (1 - j))
+                    drawLine(0f, 0f, 0f, -size * 0.5f * dsc(0), paint)
+                }
+            }
+        }
+        drawArc(RectF(-size, -size, size, size), 90f, 180f * dsc(3), false, paint)
+    }
+}
+
+fun Canvas.drawRLHANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawRotLineHalfArc(scale, w, h, paint)
+}
