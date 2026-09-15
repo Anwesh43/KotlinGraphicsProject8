513f93e326960d872e4691fb41d86908c595f88a
Anwesh43 <anweshthecool0@gmail.com>
2026-09-15 09:44:55 +0530

    rotlinehalfarcview : adding RLHANode class

--- rotlinehalfarcview/src/main/java/com/example/rotlinehalfarcview/RotLineHalfArcView.kt
+++ rotlinehalfarcview/src/main/java/com/example/rotlinehalfarcview/RotLineHalfArcView.kt
@@ -126,4 +126,45 @@ class RotLineHalfArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RLHANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RLHANode? = null
+        private var prev : RLHANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RLHANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRLHANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RLHANode {
+            var curr : RLHANode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
