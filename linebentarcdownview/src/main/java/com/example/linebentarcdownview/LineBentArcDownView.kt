65296518af3942aa3dd58cf81b0964f0bbeee588
Anwesh43 <anweshthecool0@gmail.com>
2026-09-17 19:24:58 +0530

    linebentarcdownview: adding Renderer class

--- linebentarcdownview/src/main/java/com/example/linebentarcdownview/LineBentArcDownView.kt
+++ linebentarcdownview/src/main/java/com/example/linebentarcdownview/LineBentArcDownView.kt
@@ -187,4 +187,27 @@ class LineBentArcDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineBentArcDownView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val lbad : LineBentArcDown = LineBentArcDown(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lbad.draw(canvas, paint)
+            animator.animate {
+                lbad.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lbad.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
