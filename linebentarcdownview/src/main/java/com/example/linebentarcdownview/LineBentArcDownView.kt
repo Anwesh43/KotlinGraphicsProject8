ea7d25f8c4389bc0f698ddced3c409b83e847e75
Anwesh43 <anweshthecool0@gmail.com>
2026-09-17 18:28:39 +0530

    linebentarcdownview: adding LBADNode class

--- linebentarcdownview/src/main/java/com/example/linebentarcdownview/LineBentArcDownView.kt
+++ linebentarcdownview/src/main/java/com/example/linebentarcdownview/LineBentArcDownView.kt
@@ -123,4 +123,45 @@ class LineBentArcDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBADNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBADNode? = null
+        private var prev : LBADNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBADNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBADNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) :LBADNode {
+            var curr : LBADNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
