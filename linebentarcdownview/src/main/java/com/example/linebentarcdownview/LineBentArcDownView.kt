12a9c81c59a5f61674515757e57512d617f448ae
Anwesh43 <anweshthecool0@gmail.com>
2026-09-17 15:17:52 +0530

    linebentarcdownview: adding LineBemtArcDownView class

--- linebentarcdownview/src/main/java/com/example/linebentarcdownview/LineBentArcDownView.kt
+++ linebentarcdownview/src/main/java/com/example/linebentarcdownview/LineBentArcDownView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLBADNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineBentArcDown(scale, w, h, paint)
 }
+
+class LineBentArcDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
