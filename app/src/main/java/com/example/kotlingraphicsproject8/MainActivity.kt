f4867b7c8d767807017a2153f79f1777eba29508
Anwesh43 <anweshthecool0@gmail.com>
2026-09-18 08:29:12 +0530

    arclineextendrotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.arclineextendrotview.ArcLineExtendRotView
 import com.example.kotlingraphicsproject8.ui.theme.KotlinGraphicsProject8Theme
-import com.example.linebentarcdownview.LineBentArcDownView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        LineBentArcDownView.create(this)
+        ArcLineExtendRotView.create(this)
     }
 }
 
