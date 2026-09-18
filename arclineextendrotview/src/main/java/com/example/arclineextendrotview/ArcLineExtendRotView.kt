a2bfd19914690692ce91a5efbd8234c17662e069
Anwesh43 <anweshthecool0@gmail.com>
2026-09-18 07:36:58 +0530

    arclineextendrotview: adding ArcLineExtendRotView class

--- arclineextendrotview/src/main/java/com/example/arclineextendrotview/ArcLineExtendRotView.kt
+++ arclineextendrotview/src/main/java/com/example/arclineextendrotview/ArcLineExtendRotView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawALERNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawArcLineExtendRot(scale, w, h, paint)
 }
+
+class ArcLineExtendRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
