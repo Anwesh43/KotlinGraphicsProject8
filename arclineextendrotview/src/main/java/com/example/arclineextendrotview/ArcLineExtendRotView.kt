5fc955365300a94237c425a3e1fcb1ff75edf718
Anwesh43 <anweshthecool0@gmail.com>
2026-09-18 07:55:36 +0530

    arclineextendrotview: adding ALERNode class

--- arclineextendrotview/src/main/java/com/example/arclineextendrotview/ArcLineExtendRotView.kt
+++ arclineextendrotview/src/main/java/com/example/arclineextendrotview/ArcLineExtendRotView.kt
@@ -121,4 +121,45 @@ class ArcLineExtendRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ALERNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ALERNode? = null
+        private var prev : ALERNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ALERNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawALERNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ALERNode {
+            var curr : ALERNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
