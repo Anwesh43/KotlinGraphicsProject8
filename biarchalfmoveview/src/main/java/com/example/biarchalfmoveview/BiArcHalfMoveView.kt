8ee3c531de27fa2afb462b037faba610ffda369b
Anwesh43 <anweshthecool0@gmail.com>
2026-09-19 10:04:33 +0530

    biarchalfmoveview: adding BiArcHalfMove class

--- biarchalfmoveview/src/main/java/com/example/biarchalfmoveview/BiArcHalfMoveView.kt
+++ biarchalfmoveview/src/main/java/com/example/biarchalfmoveview/BiArcHalfMoveView.kt
@@ -167,4 +167,27 @@ class BiArcHalfMoveView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiArcHalfMove(var i : Int) {
+
+        private var curr : BAHMNode = BAHMNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
