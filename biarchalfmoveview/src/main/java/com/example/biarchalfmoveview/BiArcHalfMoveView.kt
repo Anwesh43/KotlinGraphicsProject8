8dd3c95eb0e1c5fbb7a63fe8eaeae5f9c7f2725e
Anwesh43 <anweshthecool0@gmail.com>
2026-09-19 09:58:30 +0530

    biarchalfmoveview: adding BAHMNode class

--- biarchalfmoveview/src/main/java/com/example/biarchalfmoveview/BiArcHalfMoveView.kt
+++ biarchalfmoveview/src/main/java/com/example/biarchalfmoveview/BiArcHalfMoveView.kt
@@ -126,4 +126,45 @@ class BiArcHalfMoveView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BAHMNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : BAHMNode? = null
+        private var next : BAHMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BAHMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBAHMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BAHMNode {
+            var curr : BAHMNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
