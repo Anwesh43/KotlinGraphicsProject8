8fdbc4870a4fc5b306b40f5a25f441dcf0aa2fb9
Anwesh43 <anweshthecool0@gmail.com>
2026-09-20 11:55:28 +0530

    lineextendarcrotview: adding LineExtendArcRotView class

--- lineextendarcrotview/src/main/java/com/example/lineextendarcrotview/LineExtendArcRotView.kt
+++ lineextendarcrotview/src/main/java/com/example/lineextendarcrotview/LineExtendArcRotView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLEARNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineExtendArcRot(scale, w, h, paint)
 }
+
+class LineExtendArcRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
