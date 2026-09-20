7e953448de1b61dd9f149e3c5ee6dd7f5813ed25
Anwesh43 <anweshthecool0@gmail.com>
2026-09-20 12:04:21 +0530

    lineextendarcrotview: adding Renderer class

--- lineextendarcrotview/src/main/java/com/example/lineextendarcrotview/LineExtendArcRotView.kt
+++ lineextendarcrotview/src/main/java/com/example/lineextendarcrotview/LineExtendArcRotView.kt
@@ -188,4 +188,27 @@ class LineExtendArcRotView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineExtendArcRotView) {
+
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val lear : LineExtendArcRot = LineExtendArcRot(0)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lear.draw(canvas, paint)
+            animator.animate {
+                lear.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lear.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
