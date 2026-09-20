6c6a280d77734c284a6ac2c8865c32f4bc305f3a
Anwesh43 <anweshthecool0@gmail.com>
2026-09-20 12:06:40 +0530

    lineextendarcrotview: adding view in activity

--- app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
+++ app/src/main/java/com/example/kotlingraphicsproject8/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.biarchalfmoveview.BiArcHalfMoveView
 import com.example.kotlingraphicsproject8.ui.theme.KotlinGraphicsProject8Theme
+import com.example.lineextendarcrotview.LineExtendArcRotView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        BiArcHalfMoveView.create(this)
+        LineExtendArcRotView.create(this)
     }
 }
 
