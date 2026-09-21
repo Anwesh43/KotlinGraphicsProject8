a330db3375919428856276381ca3a898fbe32027
Anwesh43 <anweshthecool0@gmail.com>
2026-09-21 08:15:22 +0530

    lineexpanddividerightview: adding LEDRNode class

--- lineexpanddividerightview/src/main/java/com/example/lineexpanddividerightview/LineExpandDivideRightView.kt
+++ lineexpanddividerightview/src/main/java/com/example/lineexpanddividerightview/LineExpandDivideRightView.kt
@@ -125,4 +125,45 @@ class LineExpandDivideRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LEDRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LEDRNode? = null
+        private var prev : LEDRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LEDRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLEDRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LEDRNode {
+            var curr : LEDRNode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
