f28f523e75e35e67ffe7939b33e060c1cd591904
Anwesh43 <anweshthecool0@gmail.com>
2026-09-21 08:07:20 +0530

    lineexpanddividerightview: adding LineExpandDivideRightView class

--- lineexpanddividerightview/src/main/java/com/example/lineexpanddividerightview/LineExpandDivideRightView.kt
+++ lineexpanddividerightview/src/main/java/com/example/lineexpanddividerightview/LineExpandDivideRightView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawLEDRNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineExpandDivideRight(scale, w, h, paint)
 }
+
+class LineExpandDivideRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
