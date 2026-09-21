991fc7a21afc0df5836a1ad274388e4c206c7a0c
Anwesh43 <anweshthecool0@gmail.com>
2026-09-21 08:19:16 +0530

    lineexpanddividerightview: adding LineExpandDivideRight class

--- lineexpanddividerightview/src/main/java/com/example/lineexpanddividerightview/LineExpandDivideRightView.kt
+++ lineexpanddividerightview/src/main/java/com/example/lineexpanddividerightview/LineExpandDivideRightView.kt
@@ -166,4 +166,27 @@ class LineExpandDivideRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineExpandDivideRight(var i : Int) {
+
+        private var curr : LEDRNode = LEDRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun udpate(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
