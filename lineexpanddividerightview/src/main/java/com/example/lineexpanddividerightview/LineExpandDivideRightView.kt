cc393b225b1883f55e345c483afe14ebc19baab5
Anwesh43 <anweshthecool0@gmail.com>
2026-09-21 08:04:58 +0530

    lineexpanddividerightview: adding Canvas extension methods

--- lineexpanddividerightview/src/main/java/com/example/lineexpanddividerightview/LineExpandDivideRightView.kt
+++ lineexpanddividerightview/src/main/java/com/example/lineexpanddividerightview/LineExpandDivideRightView.kt
@@ -23,7 +23,42 @@ val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
 val rot : Float = 90f
+val splitDeg : Float = 45f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineExpandDivideRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        rotate(rot * dsc(3))
+        drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        for (j in 0..1) {
+            drawXY(0f, -size) {
+                rotate(splitDeg * (1f - 2 * j) * dsc(2))
+                drawLine(0f, 0f, size * dsc(1), 0f, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLEDRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawLineExpandDivideRight(scale, w, h, paint)
+}
