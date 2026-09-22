c8e66fc110da68102cf210442a0c9b3093706a37
Anwesh43 <anweshthecool0@gmail.com>
2026-09-22 12:31:03 +0530

    bicircledropaltview: adding Canvas extension methods

--- bicircledropaltview/src/main/java/com/example/bicircledropaltview/BiCircleDropAltView.kt
+++ bicircledropaltview/src/main/java/com/example/bicircledropaltview/BiCircleDropAltView.kt
@@ -23,8 +23,41 @@ val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
 val strokeFactor : Float = 90f
+val deg : Float = 180f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiCircleDropAlt(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(3))
+        for (j in 0..1) {
+            drawXY(-w * 0.5f * (1f - 2 * j) * dsc(4), -h * 0.5f * (1 - dsc(1))) {
+                rotate(deg * dsc(2))
+                drawArc(RectF(-size / 6, 0f, size / 6, size / 3), -90f, 360f * dsc(0), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawBCDANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawBiCircleDropAlt(scale, w, h, paint)
+}
