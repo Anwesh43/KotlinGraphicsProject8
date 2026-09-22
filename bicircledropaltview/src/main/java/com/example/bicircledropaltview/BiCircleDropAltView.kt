841bfa8ddcd863ee0dfd5e23a06ebd1faa681395
Anwesh43 <anweshthecool0@gmail.com>
2026-09-22 16:24:40 +0530

    bicircledropaltview: adding BCDANode class

--- bicircledropaltview/src/main/java/com/example/bicircledropaltview/BiCircleDropAltView.kt
+++ bicircledropaltview/src/main/java/com/example/bicircledropaltview/BiCircleDropAltView.kt
@@ -124,4 +124,45 @@ class BiCircleDropAltView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BCDANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BCDANode? = null
+        private var prev : BCDANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BCDANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBCDANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BCDANode {
+            var curr : BCDANode? = prev
+            if (dir === 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
