fb2b1a134e9fe29758d72329bd0bc7eb9e4cc725
Anwesh43 <anweshthecool0@gmail.com>
2026-09-23 06:07:58 +0530

    sermiarcrotupview: adding Canvas extension methods

--- semiarcrotupview/src/main/java/com/example/semiarcrotupview/SemiArcRotUpView.kt
+++ semiarcrotupview/src/main/java/com/example/semiarcrotupview/SemiArcRotUpView.kt
@@ -23,8 +23,43 @@ val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val backColor : Int = "#BDBDBD".toColorInt()
 val rot : Float = 90f
+val deg : Float = 180f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSemiArcRotUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h * 0.5f * (1 - dsc(4))) {
+        rotate(rot * dsc(3))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(deg * dsc(2))
+                drawXY(-2 * size * (1 - j), 0f) {
+                    drawArc(RectF(-2 * size, 0f, 0f, 2 * size), -90f + 180f * j, 90f * dsc(j), false, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawSARUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawSemiArcRotUp(scale, w, h, paint)
+}
