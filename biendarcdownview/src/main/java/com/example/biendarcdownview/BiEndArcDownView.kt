69a915dee0e89afd2f2f6558cae92abcd031755e
Anwesh43 <anweshthecool0@gmail.com>
2026-09-24 15:20:58 +0530

    biendarcdownview: adding BiEndArcDownView class

--- biendarcdownview/src/main/java/com/example/biendarcdownview/BiEndArcDownView.kt
+++ biendarcdownview/src/main/java/com/example/biendarcdownview/BiEndArcDownView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawBEADNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBiEndArcDown(scale, w, h, paint)
 }
+
+class BiEndArcDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
