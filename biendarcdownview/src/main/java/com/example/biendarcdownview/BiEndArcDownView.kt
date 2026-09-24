3b931e8b3f20c62a067a14e647bb71e81ed927cd
Anwesh43 <anweshthecool0@gmail.com>
2026-09-24 15:19:18 +0530

    biendarcdownview: adding Canvas extension methods

--- biendarcdownview/src/main/java/com/example/biendarcdownview/BiEndArcDownView.kt
+++ biendarcdownview/src/main/java/com/example/biendarcdownview/BiEndArcDownView.kt
@@ -27,3 +27,39 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int,  n: Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiEndArcDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(4)) {
+        rotate(rot * dsc(2))
+        drawLine(0f, 0f, 0f, -size * dsc(3), paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f -2  * j, 1f)
+                drawXY(-w * 0.5f * (1 - dsc(1)), 0f) {
+                    drawArc(RectF(0f, -size, 2 * size, size), 180f, 90f * dsc(0), false, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawBEADNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i].toColorInt()
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    drawBiEndArcDown(scale, w, h, paint)
+}
