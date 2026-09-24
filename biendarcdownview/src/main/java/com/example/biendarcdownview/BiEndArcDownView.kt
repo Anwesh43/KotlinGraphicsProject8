679a2efad29f981a728e5583b963b7ef891e7908
Anwesh43 <anweshthecool0@gmail.com>
2026-09-24 15:35:52 +0530

    biendarcdownview: adding Renderer class

--- biendarcdownview/src/main/java/com/example/biendarcdownview/BiEndArcDownView.kt
+++ biendarcdownview/src/main/java/com/example/biendarcdownview/BiEndArcDownView.kt
@@ -190,4 +190,27 @@ class BiEndArcDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BiEndArcDownView) {
+
+        private var bead : BiEndArcDown = BiEndArcDown(0)
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            bead.draw(canvas, paint)
+            animator.animate {
+                bead.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            bead.startUpdating {
+                animator.stop()
+            }
+        }
+    }
 }
