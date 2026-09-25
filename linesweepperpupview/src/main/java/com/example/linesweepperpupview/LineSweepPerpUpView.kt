b757f1ee057b63c3a00bb17daaa073e86f97ab23
Anwesh43 <anweshthecool0@gmail.com>
2026-09-25 14:35:27 +0530

    linesweepperpupview: adding Renderer in View

--- linesweepperpupview/src/main/java/com/example/linesweepperpupview/LineSweepPerpUpView.kt
+++ linesweepperpupview/src/main/java/com/example/linesweepperpupview/LineSweepPerpUpView.kt
@@ -65,13 +65,16 @@ fun Canvas.drawLSPUNode(i : Int, scale : Float, paint : Paint) {
 
 class LineSweepPerpUpView(ctx : Context) : View(ctx) {
 
+    private val renderer : Renderer = Renderer(this)
+
     override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
