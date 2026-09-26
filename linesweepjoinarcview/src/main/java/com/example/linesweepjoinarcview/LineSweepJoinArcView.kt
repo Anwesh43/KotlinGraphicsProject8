e8d72f1c403dca7204d464de1edf3f87842217c2
Anwesh43 <anweshthecool0@gmail.com>
2026-09-26 14:25:48 +0530

    linesweepjoinarcview: adding LineSweepJoinArcView class

--- linesweepjoinarcview/src/main/java/com/example/linesweepjoinarcview/LineSweepJoinArcView.kt
+++ linesweepjoinarcview/src/main/java/com/example/linesweepjoinarcview/LineSweepJoinArcView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawLSJANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineSweepJoinArc(scale, w, h, paint)
 }
+
+class LineSweepJoinArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
